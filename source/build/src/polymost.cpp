b04380fa0f026ed1b1e7d78fd503b6a054b823d1
nukeykt <alexeytf2@icloud.com>
2019-03-08 03:18:14 +0900

    Better fix

--- source/build/src/polymost.cpp
+++ source/build/src/polymost.cpp
@@ -4304,12 +4304,14 @@ static void polymost_internal_nonparallaxed(vec2f_t n0, vec2f_t n1, float ryp0, 
     if (globalorientation&2) //slopes
     {
         //Pick some point guaranteed to be not collinear to the 1st two points
-        vec2f_t oxy = { n0.x + (n1.y - n0.y), n0.y + (n0.x - n1.x) };
+        vec2f_t dxy = { n1.y - n0.y, n0.x - n1.x };
 
-        float const oxyr = polymost_invsqrt_approximation(oxy.x * oxy.x + oxy.y * oxy.y);
+        float const dxyr = polymost_invsqrt_approximation(dxy.x * dxy.x + dxy.y * dxy.y);
 
-        oxy.x *= oxyr;
-        oxy.y *= oxyr;
+        dxy.x *= dxyr*4096.f;
+        dxy.y *= dxyr*4096.f;
+
+        vec2f_t const oxy = { n0.x + dxy.x, n0.y + dxy.y };
 
         float const ox2 = (oxy.y - fglobalposy) * gcosang - (oxy.x - fglobalposx) * gsinang;
         float oy2 = 1.f / ((oxy.x - fglobalposx) * gcosang2 + (oxy.y - fglobalposy) * gsinang2);
