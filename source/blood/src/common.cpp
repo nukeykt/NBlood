41b6a7c40275f3e90470d348ab335b34e245e97b
nukeykt <alexeytf2@icloud.com>
2019-08-10 03:25:44 +0900

    Always load widescreen def file

--- source/blood/src/common.cpp
+++ source/blood/src/common.cpp
@@ -245,6 +245,7 @@ void G_LoadGroups(int32_t autoload)
         }
     }
 
+    loaddefinitions_game(BLOODWIDESCREENDEF, TRUE);
     loaddefinitions_game(G_DefFile(), TRUE);
 
     struct strllist *s;
