6c98208f2010387f2651ddd9343dcf45aacd1089
Richard Gobeille <terminx@gmail.com>
2019-08-14 14:26:48 +0000

    This timing stuff actually works
    It doesn't help when it turns out the compiler is broken and implementing std::chrono::high_resolution_clock as something that only counts in ms instead of properly aliasing it to std::chrono::steady_clock!
    
    From-SVN: r8002

--- source/build/src/sdlayer.cpp
+++ source/build/src/sdlayer.cpp
@@ -92,7 +92,7 @@ char nogl=0;
 static int32_t vsync_renderlayer;
 int32_t maxrefreshfreq=0;
 #if SDL_MAJOR_VERSION==2
-static uint64_t currentVBlankInterval;
+static double currentVBlankInterval;
 #endif
 
 // last gamma, contrast, brightness
@@ -1602,7 +1602,7 @@ void setrefreshrate(void)
     if (!newmode.refresh_rate)
         newmode.refresh_rate = 60;
 
-    currentVBlankInterval = timerGetFreqU64()/newmode.refresh_rate;
+    currentVBlankInterval = timerGetFreqU64()/(double)newmode.refresh_rate;
 }
 
 int32_t videoSetMode(int32_t x, int32_t y, int32_t c, int32_t fs)
@@ -1867,10 +1867,11 @@ void videoShowFrame(int32_t w)
         SDL_GL_SwapWindow(sdl_window);
         if (vsync)
         {
-            static uint64_t lastSwapTime = 0;
+            static double lastSwapTime;
             // busy loop until we're ready to update again
-            uint64_t swapTime = timerGetTicksU64();
-            while (timerGetTicksU64()-lastSwapTime < currentVBlankInterval) {}
+            // sit on it and spin
+            double swapTime = timerGetTicksU64();
+            while ((double)timerGetTicksU64()-lastSwapTime < currentVBlankInterval) {}
             lastSwapTime = swapTime;
         }
         return;
