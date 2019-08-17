4f1189630b325289bcbf9bb3069cd02ad18ac0ec
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2019-08-17 20:54:26 +0200

    revert behavioral changes and keep the cosmetic change only

--- source/blood/src/ai.cpp
+++ source/blood/src/ai.cpp
@@ -990,11 +990,11 @@ int aiDamageSprite(spritetype *pSprite, XSPRITE *pXSprite, int nSource, DAMAGE_T
                 evKill(nSprite, 3, CALLBACK_ID_0);
             }
             break;
-        case 245:
+        case 245: // innocent
             if (nDmgType == DAMAGE_TYPE_1 && pXSprite->health <= (unsigned int)pDudeInfo->fleeHealth/* && (pXSprite->at17_6 != 1 || pXSprite->at17_6 != 2)*/)
             {
                 pSprite->type = 239;
-                aiNewState(pSprite, pXSprite, &innocentBurnGoto);
+                aiNewState(pSprite, pXSprite, &cultistBurnGoto);
                 aiPlay3DSound(pSprite, 361, AI_SFX_PRIORITY_0, -1);
                 gDudeExtra[pSprite->extra].at0 = gFrameClock+360;
                 actHealDude(pXSprite, dudeInfo[39].startHealth, dudeInfo[39].startHealth);
@@ -1039,8 +1039,8 @@ int aiDamageSprite(spritetype *pSprite, XSPRITE *pXSprite, int nSource, DAMAGE_T
             {
                 pSprite->type = 239;
                 if (!VanillaMode())
-                    pXSprite->scale = -4;
-                aiNewState(pSprite, pXSprite, &tinycalebBurnGoto);
+                    pXSprite->scale = -4; // need to change this to 64 later
+                aiNewState(pSprite, pXSprite, &cultistBurnGoto);
                 aiPlay3DSound(pSprite, 361, AI_SFX_PRIORITY_0, -1);
                 gDudeExtra[pSprite->extra].at0 = gFrameClock+360;
                 actHealDude(pXSprite, dudeInfo[39].startHealth, dudeInfo[39].startHealth);
