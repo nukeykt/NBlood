e570a4056711fe1a9f56731f5e56f29031fb172b
CommonLoon102 <CommonLoon102@users.noreply.github.com>
2019-09-24 00:47:15 +0200

    reverb effect bugfix

--- source/blood/src/player.cpp
+++ source/blood/src/player.cpp
@@ -496,7 +496,7 @@ char powerupActivate(PLAYER *pPlayer, int nPowerUp)
         pPlayer->ata1[0]++;
         break;
     case 124: // reflective shots
-        if (pPlayer == gMe && VanillaMode() ? gGameOptions.nGameType == 0 : true)
+        if (pPlayer == gMe && gGameOptions.nGameType == 0)
             sfxSetReverb2(1);
         break;
     case 114: // death mask
@@ -505,7 +505,7 @@ char powerupActivate(PLAYER *pPlayer, int nPowerUp)
         break;
     case 118: // diving suit
         pPlayer->ata1[4]++;
-        if (pPlayer == gMe && VanillaMode() ? gGameOptions.nGameType == 0 : true)
+        if (pPlayer == gMe && gGameOptions.nGameType == 0)
             sfxSetReverb(1);
         break;
     case 119:
