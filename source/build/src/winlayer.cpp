b5c7b94289eef988d8d386ba6dff7ae370b3519b
Richard Gobeille <terminx@gmail.com>
2019-10-19 23:42:55 +0000

    This old 3dfx and SiS hardware isn't capable of running our GL renderer and never will be
    From-SVN: r8159

--- source/build/src/winlayer.cpp
+++ source/build/src/winlayer.cpp
@@ -2676,8 +2676,6 @@ static int32_t SetupOpenGL(int32_t width, int32_t height, int32_t bitspp)
         // GL driver blacklist
 
         if (!Bstrcmp(glinfo.vendor,"Microsoft Corporation")) err = 1;
-        else if (!Bstrcmp(glinfo.vendor,"SiS")) err = 1;
-        else if (!Bstrcmp(glinfo.vendor,"3Dfx Interactive Inc.")) err = 1;
 #ifdef POLYMER
         else if (!Bstrcmp(glinfo.vendor, "Intel"))
             pr_ati_fboworkaround = 1;
