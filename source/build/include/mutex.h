nukeykt/NBlood

commit a6bc810faab6a4ae57a4808061f7ca90a9a58e28

Re-implement our mutex_lock() family of functions using SDL's atomic spinlocks
From-SVN: r7907

source/build/include/mutex.h
@@ -19,12 +19,12 @@ typedef HANDLE mutex_t;
 #else
 /* PK: I don't like pointer typedefs, but SDL_CreateMutex() _returns_ one,
  *     so we're out of luck with our interface. */
-typedef SDL_mutex* mutex_t;
+typedef SDL_SpinLock mutex_t;
 #endif
 
 extern int32_t mutex_init(mutex_t *mutex);
-extern int32_t mutex_lock(mutex_t *mutex);
-extern int32_t mutex_unlock(mutex_t *mutex);
+extern void mutex_lock(mutex_t *mutex);
+extern void mutex_unlock(mutex_t *mutex);
 
 
 #ifdef __cplusplus

source/build/src/mutex.cpp
@@ -13,30 +13,25 @@ int32_t mutex_init(mutex_t *mutex)
     *mutex = CreateMutex(0, FALSE, 0);
     return (*mutex == 0);
 #else
-    if (mutex)
-    {
-        *mutex = SDL_CreateMutex();
-        if (*mutex != NULL)
-            return 0;
-    }
-    return -1;
+    *mutex = 0;
+    return 0;
 #endif
 }
 
-int32_t mutex_lock(mutex_t *mutex)
+void mutex_lock(mutex_t *mutex)
 {
 #ifdef RENDERTYPEWIN
-    return (WaitForSingleObject(*mutex, INFINITE) == WAIT_FAILED);
+    return WaitForSingleObject(*mutex, INFINITE);
 #else
-    return SDL_LockMutex(*mutex);
+    return SDL_AtomicLock(mutex);
 #endif
 }
 
-int32_t mutex_unlock(mutex_t *mutex)
+void mutex_unlock(mutex_t *mutex)
 {
 #ifdef RENDERTYPEWIN
-    return (ReleaseMutex(*mutex) == 0);
+    ReleaseMutex(*mutex);
 #else
-    return SDL_UnlockMutex(*mutex);
+    SDL_AtomicUnlock(mutex);
 #endif
 }

commit 634f5eefd174c3e2f2fd37fe359386e878ec119f

Fix mutex implementation with SDL 1.2, broken in r7907
From-SVN: r8253

source/build/include/mutex.h
@@ -16,9 +16,9 @@ extern "C" {
 
 #ifdef RENDERTYPEWIN
 typedef HANDLE mutex_t;
+#elif SDL_MAJOR_VERSION == 1
+typedef SDL_mutex * mutex_t;
 #else
-/* PK: I don't like pointer typedefs, but SDL_CreateMutex() _returns_ one,
- *     so we're out of luck with our interface. */
 typedef SDL_SpinLock mutex_t;
 #endif
 

source/build/src/mutex.cpp
@@ -12,6 +12,14 @@ int32_t mutex_init(mutex_t *mutex)
 #ifdef RENDERTYPEWIN
     *mutex = CreateMutex(0, FALSE, 0);
     return (*mutex == 0);
+#elif SDL_MAJOR_VERSION == 1
+    if (mutex)
+    {
+        *mutex = SDL_CreateMutex();
+        if (*mutex != NULL)
+            return 0;
+    }
+    return -1;
 #else
     *mutex = 0;
     return 0;
@@ -22,6 +30,8 @@ void mutex_lock(mutex_t *mutex)
 {
 #ifdef RENDERTYPEWIN
     WaitForSingleObject(*mutex, INFINITE);
+#elif SDL_MAJOR_VERSION == 1
+    SDL_LockMutex(*mutex);
 #else
     SDL_AtomicLock(mutex);
 #endif
@@ -31,6 +41,8 @@ void mutex_unlock(mutex_t *mutex)
 {
 #ifdef RENDERTYPEWIN
     ReleaseMutex(*mutex);
+#elif SDL_MAJOR_VERSION == 1
+    SDL_UnlockMutex(*mutex);
 #else
     SDL_AtomicUnlock(mutex);
 #endif