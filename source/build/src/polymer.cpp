f462e8cf23f391efe6872df100944f4445f1d5f7
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2019-12-26 06:27:58 +0000

    Refactor tsprite creation into renderAddTSpriteFromSprite
    git-svn-id: https://svn.eduke32.com/eduke32@8521 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/src/polymer.cpp
+++ source/build/src/polymer.cpp
@@ -3927,9 +3927,7 @@ static inline void  polymer_scansprites(int16_t sectnum, tspriteptr_t localtspri
         {
             // this function's localtsprite is either the tsprite global or
             // polymer_drawroom's locattsprite, so no aliasing
-            Bmemcpy(&localtsprite[*localspritesortcnt], spr, sizeof(tspritetype));
-            localtsprite[*localspritesortcnt].extra = 0;
-            localtsprite[(*localspritesortcnt)++].owner = i;
+            renderMakeTSpriteFromSprite(&localtsprite[(*localspritesortcnt)++], i);
         }
     }
 }
