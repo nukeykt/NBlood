4c9b33c30d283001fc0ae659d347018b8eed7384
nukeykt <alexeytf2@icloud.com>
2020-01-02 21:10:14 +0900

    Blood: fix uninitialized palette usage

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -3507,7 +3507,7 @@ RORHACK:
         {
             if (gAimReticle)
             {
-                rotatesprite(160<<16, defaultHoriz<<16, 65536, 0, kCrosshairTile, 0, CROSSHAIR_PAL, 2, gViewX0, gViewY0, gViewX1, gViewY1);
+                rotatesprite(160<<16, defaultHoriz<<16, 65536, 0, kCrosshairTile, 0, g_isAlterDefaultCrosshair ? CROSSHAIR_PAL : 0, 2, gViewX0, gViewY0, gViewX1, gViewY1);
             }
             cX = (v4c>>8)+160;
             cY = (v48>>8)+220+(zDelta>>7);
