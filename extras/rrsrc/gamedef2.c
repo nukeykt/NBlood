13c78be6ec3cd7faeff2633a1d9c62fa29efc749
NY00123 <ny00@outlook.com>
2020-01-31 01:20:27 +0200

    Clean-up the changes to extras/rrsrc/gamedef2.c:parse

--- extras/rrsrc/gamedef2.c
+++ extras/rrsrc/gamedef2.c
@@ -1311,34 +1311,15 @@ char parse(void)
         case 115:
             insptr++;
             {
-#define B0 spritetype *js;
-#define B1 short lotag, hitag;
-#define B2 short k, jj;
-#define B3 short wi, wj;
-#define B4 short nextk, nextjj;
-#define B5 short spr;
-#define B6 short wallstart, wallend;
-#define B7 short wallstart2, wallend2;
-#define B8 short sectnum;
-//012354867.LST
-#define A0 B0
-#define A1 B1
-#define A2 B2
-#define A3 B3
-#define A5 B4
-#define A4 B5
-#define A8 B6
-#define A6 B7
-#define A7 B8
-                A0
-                A1
-                A2
-                A3
-                A4
-                A5
-                A6
-                A7
-                A8
+                spritetype *js;
+                short lotag, hitag;
+                short k, jj;
+                short wi, wj;
+                short spr;
+                short nextk, nextjj;
+                short wallstart2, wallend2;
+                short sectnum;
+                short wallstart, wallend;
 
                 hitag = 0;
                 k = headspritesect[g_sp->sectnum];
