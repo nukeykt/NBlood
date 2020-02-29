18953af9a5b50c5253ee6f3f0b779ddf8e5031b5
hendricks266 <hendricks266@1a8010ca-5511-0410-912e-c29ae57300e0>
2020-02-29 07:35:53 +0000

    Polymost: Fix flipped HUD model rendering
    Patch from Nuke.YKT.
    
    git-svn-id: https://svn.eduke32.com/eduke32@8645 1a8010ca-5511-0410-912e-c29ae57300e0

--- source/build/src/polymost.cpp
+++ source/build/src/polymost.cpp
@@ -8647,8 +8647,6 @@ void polymost_dorotatespritemodel(int32_t sx, int32_t sy, int32_t z, int16_t a, 
     float const oldfviewingrange = fviewingrange; fviewingrange = 65536.f;
     float const ogvrcorrection = gvrcorrection; gvrcorrection = 1.f;
 
-    polymost_updaterotmat();
-
     vec1 = hud->add;
 
 #ifdef POLYMER
@@ -8847,7 +8845,6 @@ void polymost_dorotatespritemodel(int32_t sx, int32_t sy, int32_t z, int16_t a, 
     gshang = ogshang;
     gctang = ogctang;
     gstang = ogstang;
-    polymost_identityrotmat();
 }
 
 void polymost_dorotatesprite(int32_t sx, int32_t sy, int32_t z, int16_t a, int16_t picnum,
