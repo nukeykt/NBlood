d7c6da09d319a807e15def24a30e509e5ac1ba89
NY00123 <ny00@outlook.com>
2020-03-03 22:55:13 +0000

    kenbuild/src/game.cpp: Import mirror fix for SW from SVN r8588.
    From-SVN: r8671

--- source/kenbuild/src/game.cpp
+++ source/kenbuild/src/game.cpp
@@ -4045,7 +4045,7 @@ void drawscreen(short snum, int dasmoothratio)
                         //position into tposx, tposy, and tang (tpos.z == cpos.z)
                         //Must call renderPrepareMirror before drawrooms and
                         //          renderCompleteMirror after drawrooms
-                        renderPrepareMirror(cpos.x,cpos.y,cpos.z,fix16_from_int(cang),choriz,
+                        renderPrepareMirror(cpos.x,cpos.y,cpos.z,fix16_from_int(cang),fix16_from_int(choriz),
                                       mirrorwall[i],/*mirrorsector[i],*/ &tposx,&tposy,&tang);
 
                         ovisibility = g_visibility;
