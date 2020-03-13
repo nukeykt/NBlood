e761f658ebfd93c28445c417af5e945dbd54d371
NY00123 <ny00@outlook.com>
2020-03-13 15:28:43 +0000

    SW: Fix a bug in getpackets, making SW's Master-Slave code usable, including 3+ players sessions.
    From-SVN: r8743

--- source/sw/src/game.cpp
+++ source/sw/src/game.cpp
@@ -923,15 +923,7 @@ InitGame(int32_t argc, char const * const * argv)
         initsingleplayers();
     else if (initmultiplayersparms(argc - firstnet, &argv[firstnet]))
     {
-        if ((networkmode == MMULTI_MODE_MS) && (numplayers > 2))
-        {
-            QuitFlag = TRUE;
-            wm_msgbox("Multiplayer Option Error",
-                      "This release does not support a master-slave networking "
-                      "mode for more than 2 players. However, peer-to-peer "
-                      "networking has been found to be playable.");
-            return;
-        }
+        NetBroadcastMode = (networkmode == MMULTI_MODE_P2P);
         buildputs("Waiting for players...\n");
         while (initmultiplayerscycle())
         {
