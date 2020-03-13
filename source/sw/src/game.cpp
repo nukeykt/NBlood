56eea83a96b25cbd16f75c191fa4ad5a8119d761
NY00123 <ny00@outlook.com>
2020-03-13 15:28:48 +0000

    sw/src/game.cpp:NewLevel: Wait for all peers when master player quits.
    From-SVN: r8746

--- source/sw/src/game.cpp
+++ source/sw/src/game.cpp
@@ -1674,7 +1674,9 @@ NewLevel(void)
         MONO_PRINT(ds);
         RunLevel();
 
-        if (!QuitFlag)
+        // On map change, or after master player quits
+        if (!QuitFlag ||
+            (!NetBroadcastMode && TEST_SYNC_KEY(Player + connecthead, SK_QUIT_GAME)))
         {
             // for good measure do this
             ready2send = 0;
