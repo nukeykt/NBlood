911159947cf17d3cfc8b1c901d693fb2e3406082
Richard Gobeille <terminx@gmail.com>
2020-03-17 03:19:24 +0000

    The most minor of audiolib cleanups
    From-SVN: r8757

--- source/audiolib/src/drivers.cpp
+++ source/audiolib/src/drivers.cpp
@@ -1,6 +1,7 @@
 /*
  Copyright (C) 2009 Jonathon Fowler <jf@jonof.id.au>
- 
+ Copyright (C) EDuke32 developers and contributors
+
  This program is free software; you can redistribute it and/or
  modify it under the terms of the GNU General Public License
  as published by the Free Software Foundation; either version 2
