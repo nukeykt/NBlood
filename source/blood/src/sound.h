0e42143f079b42106448acebfedb5893bf9fd1e8
sirlemonhead <sirlemonhead@outlook.com>
2020-04-22 16:23:28 +0100

    Blood: rename SAMPLE2D struct var, at0 -> hVoice

--- source/blood/src/sound.h
+++ source/blood/src/sound.h
@@ -25,7 +25,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "resource.h"
 struct SAMPLE2D
 {
-    int at0;
+    int hVoice;
     char at4;
     DICTNODE *at5;
 }; // 9 bytes
