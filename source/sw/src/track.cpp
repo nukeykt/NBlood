07df0ebb7c7680a6b307527f72135efa6f3ae9a7
NY00123 <ny00@outlook.com>
2020-05-03 00:20:48 +0300

    SW: Afraid that we should disable almost all kinds of SOs in multiplayer for now, due to possible jitters. Currently leaving remote-controlled SOs.

--- source/sw/src/track.cpp
+++ source/sw/src/track.cpp
@@ -1631,7 +1631,7 @@ MovePlayer(PLAYERp pp, SECTOR_OBJECTp sop, int nx, int ny)
 
     if (TEST(sop->flags, SOBJ_DONT_ROTATE))
     {
-        if (!gs.InterpolateSO)
+        if (!gs.InterpolateSO || CommEnabled)
         {
             pp->oposx = pp->posx;
             pp->oposy = pp->posy;
@@ -1681,7 +1681,7 @@ MovePlayer(PLAYERp pp, SECTOR_OBJECTp sop, int nx, int ny)
     pp->camq16ang = NORM_Q16ANGLE(pp->camq16ang);
     pp->q16ang = NORM_Q16ANGLE(pp->RevolveQ16Ang + fix16_from_int(pp->RevolveDeltaAng));
 
-    if (!gs.InterpolateSO)
+    if (!gs.InterpolateSO || CommEnabled)
     {
         pp->oq16ang = pp->q16ang;
         pp->oposx = pp->posx;
@@ -1941,7 +1941,7 @@ PlayerPart:
                     pp->SpriteP->z = pp->loz;
                 }
             }
-            if (!gs.InterpolateSO)
+            if (!gs.InterpolateSO || CommEnabled)
                 pp->oposz = pp->posz;
         }
         else
