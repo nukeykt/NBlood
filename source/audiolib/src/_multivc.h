nukeykt/NBlood

commit 09dc0cc9489d33e86ddd5f1605014b63cb612139

audiolib: cleanups and consolidations
Zeroing of fields when starting playback moved to common functions, revised allocation of memory for FLAC, Vorbis, XA, and XMP, addition of "snd_lazyalloc" cvar to control reusal of memory blocks allocated for Vorbis, XA, and XMP playback, other misc code cleanup.

source/audiolib/src/_multivc.h
@@ -57,70 +57,35 @@ extern struct VoiceNode  VoiceList;
 extern struct VoiceNode  VoicePool;
 
 extern fix16_t MV_GlobalVolume;
-extern fix16_t MV_VolumeSmooth;
+extern fix16_t MV_VolumeSmoothFactor;
 
-static FORCE_INLINE fix16_t SMOOTH_VOLUME(fix16_t const volume, fix16_t const dest)
-{
-    return volume + fix16_fast_trunc_mul(dest - volume, MV_VolumeSmooth);
-}
+static FORCE_INLINE fix16_t SMOOTH_VOLUME(fix16_t const volume, fix16_t const dest) { return volume + fix16_fast_trunc_mul(dest - volume, MV_VolumeSmoothFactor); }
 
-template <typename T>
-static inline conditional_t< is_signed<T>::value, make_unsigned_t<T>, make_signed_t<T> > FLIP_SIGN(T src)
+template <typename T> static inline conditional_t<is_signed<T>::value, make_unsigned_t<T>, make_signed_t<T>> FLIP_SIGN(T src)
 {
     static constexpr make_unsigned_t<T> msb = ((make_unsigned_t<T>)1) << (sizeof(T) * CHAR_BIT - 1u);
     return src ^ msb;
 }
 
-template <typename T>
-static inline enable_if_t<is_signed<T>::value, T> SCALE_SAMPLE(T src, fix16_t volume)
+template <typename T> static inline enable_if_t<is_signed<T>::value, T> SCALE_SAMPLE(T src, fix16_t volume)
 {
     return (T)fix16_fast_trunc_mul_int_by_fix16(src, volume);
 }
 
-template <typename T>
-static inline T CONVERT_SAMPLE_FROM_SIGNED(int src);
+template <typename T> static inline T CONVERT_SAMPLE_FROM_SIGNED(int src);
+template <> inline int16_t CONVERT_SAMPLE_FROM_SIGNED<int16_t>(int src) { return src; }
 
-template<>
-inline int16_t CONVERT_SAMPLE_FROM_SIGNED<int16_t>(int src)
-{
-    return src;
-}
+template <typename T> static inline int CONVERT_SAMPLE_TO_SIGNED(T src);
+template <> inline int CONVERT_SAMPLE_TO_SIGNED<int16_t>(int16_t src) { return src; }
 
-template <typename T>
-static inline int CONVERT_SAMPLE_TO_SIGNED(T src);
+template <typename S, typename D> static inline int CONVERT_LE_SAMPLE_TO_SIGNED(S src);
+template <> inline int CONVERT_LE_SAMPLE_TO_SIGNED<uint8_t, int16_t>(uint8_t src) { return FLIP_SIGN(src) << 8; }
+template <> inline int CONVERT_LE_SAMPLE_TO_SIGNED<int16_t, int16_t>(int16_t src) { return B_LITTLE16(src); }
 
-template<>
-inline int CONVERT_SAMPLE_TO_SIGNED<int16_t>(int16_t src)
-{
-    return src;
-}
+template <typename T> static int CLAMP_SAMPLE(int src);
+template <> inline int CLAMP_SAMPLE<int16_t>(int src) { return clamp(src, INT16_MIN, INT16_MAX); }
 
-template <typename S, typename D>
-static inline int CONVERT_LE_SAMPLE_TO_SIGNED(S src);
-
-template<>
-inline int CONVERT_LE_SAMPLE_TO_SIGNED<uint8_t, int16_t>(uint8_t src)
-{
-    return FLIP_SIGN(src) << 8;
-}
-
-template<>
-inline int CONVERT_LE_SAMPLE_TO_SIGNED<int16_t, int16_t>(int16_t src)
-{
-    return B_LITTLE16(src);
-}
-
-template <typename T>
-static int CLAMP_SAMPLE(int src);
-
-template<>
-inline int CLAMP_SAMPLE<int16_t>(int src)
-{
-    return clamp(src, INT16_MIN, INT16_MAX);
-}
-
-template <typename T>
-static T MIX_SAMPLES(int signed_sample, T untouched_sample)
+template <typename T> static T MIX_SAMPLES(int signed_sample, T untouched_sample)
 {
     return CONVERT_SAMPLE_FROM_SIGNED<T>(CLAMP_SAMPLE<T>(signed_sample + CONVERT_SAMPLE_TO_SIGNED<T>(untouched_sample)));
 }
@@ -164,9 +129,6 @@ typedef struct VoiceNode
 
     const char *sound;
 
-    fix16_t LeftVolume, LeftVolumeDest;
-    fix16_t RightVolume, RightVolumeDest;
-
     union
     {
         void *rawdataptr;
@@ -174,21 +136,33 @@ typedef struct VoiceNode
     };
 
     const char *NextBlock;
-    const char *LoopStart;
-    const char *LoopEnd;
+
+    struct
+    {
+        const char *Start;
+        const char *End;
+        int         Count;
+        uint32_t    Size;
+    } Loop;
+
+    intptr_t callbackval;
+
+    struct
+    {
+        fix16_t Left;
+        fix16_t Right;
+    } PannedVolume, GoalVolume;
 
     wavefmt_t wavetype;
-    char bits;
-    char channels;
-    char ptrlock;
+
+    int bits;
+    int channels;
 
     fix16_t volume;
 
-    int      LoopCount;
-    uint32_t LoopSize;
     uint32_t BlockLength;
 
-    int ptrlength;  // ptrlength-1 is the max permissible index for rawdataptr
+    uint32_t rawdatasiz;  // rawdatasiz-1 is the max permissible index for rawdataptr
 
     uint32_t PitchScale;
     uint32_t FixedPointBufferSize;
@@ -202,8 +176,6 @@ typedef struct VoiceNode
     int handle;
     int priority;
 
-    intptr_t callbackval;
-    void *userdata;
 } VoiceNode;
 
 typedef struct
@@ -243,6 +215,7 @@ extern int MV_Installed;
 extern int MV_MixRate;
 extern char *MV_MusicBuffer;
 extern int MV_BufferSize;
+extern int MV_LazyAlloc;
 
 extern int MV_MaxVoices;
 extern int MV_Channels;
@@ -260,7 +233,7 @@ static FORCE_INLINE int MV_SetErrorCode(int status)
 
 void MV_PlayVoice(VoiceNode *voice);
 
-VoiceNode *MV_AllocVoice(int priority);
+VoiceNode *MV_AllocVoice(int priority, uint32_t allocsize = 0);
 
 void MV_SetVoiceMixMode(VoiceNode *voice);
 void MV_SetVoiceVolume(VoiceNode *voice, int vol, int left, int right, fix16_t volume);

source/audiolib/src/multivoc.cpp
@@ -50,7 +50,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 int MV_XMPInterpolation = XMP_INTERP_NEAREST;
 #endif
 
-
 static void MV_StopVoice(VoiceNode *voice);
 static void MV_ServiceVoc(void);
 
@@ -63,7 +62,6 @@ static fix16_t MV_ReverbVolume;
 Pan MV_PanTable[MV_NUMPANPOSITIONS][MV_MAXVOLUME + 1];
 
 int MV_Installed;
-static int MV_TotalVolume = MV_MAXTOTALVOLUME;
 
 int MV_BufferSize = MV_MIXBUFFERSIZE;
 static int MV_BufferLength;
@@ -74,6 +72,8 @@ int MV_MaxVoices = 1;
 int MV_Channels = 1;
 int MV_MixRate;
 void *MV_InitDataPtr;
+
+int MV_LazyAlloc = true;
 
 #ifdef ASS_REVERSESTEREO
 static int MV_ReverseStereo;
@@ -98,7 +98,7 @@ int MV_RightChannelOffset;
 int MV_ErrorCode = MV_NotInstalled;
 
 fix16_t MV_GlobalVolume = fix16_one;
-fix16_t MV_VolumeSmooth = fix16_one;
+fix16_t MV_VolumeSmoothFactor = fix16_one;
 
 int MV_Locked;
 
@@ -135,7 +135,12 @@ static bool MV_Mix(VoiceNode * const voice, int const buffer)
         {
             if (position >= voclen - voice->channels)
             {
-                voice->GetSound(voice);
+                if (voice->GetSound(voice) != KeepPlaying)
+                {
+                    MV_GlobalVolume = gv;
+                    return false;
+                }
+
                 break;
             }
 
@@ -148,7 +153,7 @@ static bool MV_Mix(VoiceNode * const voice, int const buffer)
         if (voice->position >= voclen - voice->channels)
         {
             // Get the next block of sound
-            if (voice->GetSound(voice) == NoMoreData)
+            if (voice->GetSound(voice) != KeepPlaying)
             {
                 MV_GlobalVolume = gv;
                 return false;
@@ -168,8 +173,8 @@ void MV_PlayVoice(VoiceNode *voice)
 {
     MV_Lock();
     LL::SortedInsert(&VoiceList, voice, &VoiceNode::priority);
-    voice->LeftVolume = voice->LeftVolumeDest;
-    voice->RightVolume = voice->RightVolumeDest;
+    voice->PannedVolume = voice->GoalVolume;
+    voice->Paused = false;
     MV_Unlock();
 }
 
@@ -190,7 +195,11 @@ static void MV_CleanupVoice(VoiceNode *voice)
 #ifdef HAVE_XMP
         case FMT_XMP:    MV_ReleaseXMPVoice(voice); break;
 #endif
-        default: break;
+        default:
+            // these are in the default case of this switch instead of down below because the functions above only zero them if MV_LazyAlloc is false
+            voice->rawdataptr = nullptr;
+            voice->rawdatasiz = 0;
+            break;
     }
 
     voice->handle = 0;
@@ -406,7 +415,7 @@ int MV_VoicesPlaying(void)
     return NumVoices;
 }
 
-static VoiceNode *MV_GetLowestPriorityVoice(void)
+static inline VoiceNode *MV_GetLowestPriorityVoice(void)
 {
     auto voice = VoiceList.next;
 
@@ -420,7 +429,21 @@ static VoiceNode *MV_GetLowestPriorityVoice(void)
     return voice;
 }
 
-VoiceNode *MV_AllocVoice(int priority)
+static inline void MV_FinishAllocation(VoiceNode* voice, uint32_t const allocsize)
+{
+    if (!allocsize || (voice->rawdataptr != nullptr && voice->rawdatasiz == allocsize))
+        return;
+    else if (voice->rawdataptr != nullptr && voice->wavetype >= FMT_VORBIS)
+    {
+        // this is sort of a hack... wavetypes less than FMT_VORBIS never do their own allocations, so don't bother trying to free them
+        ALIGNED_FREE_AND_NULL(voice->rawdataptr);
+    }
+
+    voice->rawdataptr = Xaligned_calloc(16, 1, allocsize);
+    voice->rawdatasiz = allocsize;
+}
+
+VoiceNode *MV_AllocVoice(int priority, uint32_t allocsize /* = 0 */)
 {
     MV_Lock();
 
@@ -454,7 +477,13 @@ VoiceNode *MV_AllocVoice(int priority)
             vhan = MV_MINVOICEHANDLE;
     } while (MV_VoicePlaying(vhan));
 
+    voice->length = 0;
+    voice->BlockLength = 0;
     voice->handle = vhan;
+    voice->next = voice->prev = nullptr;
+
+    if (allocsize)
+        MV_FinishAllocation(voice, allocsize);
 
     return voice;
 }
@@ -572,8 +601,7 @@ void MV_SetVoiceVolume(VoiceNode *voice, int vol, int left, int right, fix16_t v
         swap(&left, &right);
 #endif
 
-    voice->LeftVolumeDest = fix16_smul(fix16_from_int(left), F16(1.f/MV_MAXTOTALVOLUME));
-    voice->RightVolumeDest = fix16_smul(fix16_from_int(right), F16(1.f/MV_MAXTOTALVOLUME));
+    voice->GoalVolume = { fix16_smul(fix16_from_int(left), F16(1.f/MV_MAXTOTALVOLUME)), fix16_smul(fix16_from_int(right), F16(1.f/MV_MAXTOTALVOLUME)) };
     voice->volume = volume;
 
     MV_SetVoiceMixMode(voice);
@@ -653,9 +681,7 @@ int MV_EndLooping(int handle)
     if (voice == nullptr)
         return MV_Error;
 
-    voice->LoopCount = 0;
-    voice->LoopStart = nullptr;
-    voice->LoopEnd = nullptr;
+    voice->Loop = {};
 
     MV_EndService();
 
@@ -711,7 +737,7 @@ static int MV_SetMixMode(int numchannels)
         return MV_Error;
 
     MV_Channels = 1 + (numchannels == 2);
-    MV_SampleSize = sizeof(int8_t) * MV_Channels * 2;
+    MV_SampleSize = sizeof(int16_t) * MV_Channels;
 
     MV_BufferSize = MV_MIXBUFFERSIZE * MV_SampleSize;
     MV_NumberOfBuffers = tabledivide32(MV_TOTALBUFFERSIZE, MV_BufferSize);
@@ -783,14 +809,9 @@ static void MV_CalcPanTable(void)
     }
 }
 
-void MV_SetVolume(int volume)
-{
-    MV_TotalVolume  = min(max(0, volume), MV_MAXTOTALVOLUME);
-    MV_GlobalVolume = fix16_smul(fix16_from_int(volume), F16(1.f/255.f));
-    // MV_CalcVolume(MV_TotalVolume);
-}
+void MV_SetVolume(int volume) { MV_GlobalVolume = fix16_smul(fix16_from_int(volume), F16(1.f/MV_MAXTOTALVOLUME)); }
 
-int MV_GetVolume(void) { return MV_TotalVolume; }
+int MV_GetVolume(void) { return Blrintf(fix16_to_float(MV_GlobalVolume) * MV_MAXTOTALVOLUME); }
 
 void MV_SetCallBack(void (*function)(intptr_t)) { MV_CallBackFunc = function; }
 
@@ -808,12 +829,7 @@ int MV_Init(int soundcard, int MixRate, int Voices, int numchannels, void *initd
 
     int const totalmem = Voices * sizeof(VoiceNode) + (MV_TOTALBUFFERSIZE * sizeof(int16_t)) + (MV_MIXBUFFERSIZE * numchannels * sizeof(int16_t));
 
-    char *ptr = (char *) Xaligned_alloc(16, totalmem);
-
-    if (!ptr)
-        return MV_SetErrorCode(MV_NoMem);
-
-    Bmemset(ptr, 0, totalmem);
+    char *ptr = (char *) Xaligned_calloc(16, 1, totalmem);
 
     MV_Voices = (VoiceNode *)ptr;
     ptr += Voices * sizeof(VoiceNode);
@@ -870,7 +886,7 @@ int MV_Init(int soundcard, int MixRate, int Voices, int numchannels, void *initd
     // Calculate pan table
     MV_CalcPanTable();
 
-    MV_VolumeSmooth = fix16_from_float(1.f-powf(0.1f, 30.f/MixRate));
+    MV_VolumeSmoothFactor = fix16_from_float(1.f-powf(0.1f, 30.f/MixRate));
 
     // Start the playback engine
     if (MV_StartPlayback() != MV_Ok)
@@ -958,8 +974,6 @@ const char *MV_ErrorString(int ErrorNumber)
             return SoundDriver_PCM_ErrorString(SoundDriver_PCM_GetError());
         case MV_NoVoices:
             return "No free voices available to Multivoc.";
-        case MV_NoMem:
-            return "Out of memory in Multivoc.";
         case MV_VoiceNotFound:
             return "No voice with matching handle found.";
         case MV_InvalidFile:
@@ -986,7 +1000,7 @@ static playbackstatus MV_GetNextDemandFeedBlock(VoiceNode* voice)
         return NoMoreData;
 
     voice->position = 0;
-    (voice->DemandFeed)(&voice->sound, &voice->BlockLength, voice->userdata);
+    (voice->DemandFeed)(&voice->sound, &voice->BlockLength, voice->rawdataptr);
     voice->length = min(voice->BlockLength, 0x8000u);
     voice->BlockLength -= voice->length;
     voice->length <<= 16;
@@ -1000,21 +1014,13 @@ static playbackstatus MV_GetNextDemandFeedBlock(VoiceNode* voice)
 int MV_StartDemandFeedPlayback(void (*function)(const char** ptr, uint32_t* length, void* userdata), int bitdepth, int channels, int rate,
     int pitchoffset, int vol, int left, int right, int priority, fix16_t volume, intptr_t callbackval, void* userdata)
 {
-    VoiceNode* voice;
-
     if (!MV_Installed)
-    {
-        MV_SetErrorCode(MV_NotInstalled);
-        return MV_Error;
-    }
+        return MV_SetErrorCode(MV_NotInstalled);
 
     // Request a voice from the voice pool
-    voice = MV_AllocVoice(priority);
-    if (voice == NULL)
-    {
-        MV_SetErrorCode(MV_NoVoices);
-        return MV_Error;
-    }
+    auto voice = MV_AllocVoice(priority);
+    if (voice == nullptr)
+        return MV_SetErrorCode(MV_NoVoices);
 
 //    voice->wavetype = FMT_DEMANDFED;
     voice->bits = bitdepth;
@@ -1022,18 +1028,14 @@ int MV_StartDemandFeedPlayback(void (*function)(const char** ptr, uint32_t* leng
     voice->GetSound = MV_GetNextDemandFeedBlock;
     voice->NextBlock  = nullptr;
     voice->DemandFeed = function;
-    voice->LoopStart  = nullptr;
-    voice->LoopEnd    = nullptr;
-    voice->LoopCount  = 0;
-    voice->BlockLength = 0;
     voice->position = 0;
     voice->sound  = nullptr;
     voice->length = 0;
-    voice->next = nullptr;
-    voice->prev = nullptr;
     voice->priority = priority;
     voice->callbackval = callbackval;
-    voice->userdata = userdata;
+    voice->rawdataptr = userdata;
+
+    voice->Loop = {};
 
     MV_SetVoicePitch(voice, rate, pitchoffset);
     MV_SetVoiceMixMode(voice);

source/audiolib/include/multivoc.h
@@ -61,7 +61,6 @@ enum MV_Errors
     MV_NotInstalled,
     MV_DriverError,
     MV_NoVoices,
-    MV_NoMem,
     MV_VoiceNotFound,
     MV_InvalidFile,
 };

source/audiolib/src/flac.cpp
@@ -224,19 +224,18 @@ FLAC__StreamDecoderWriteStatus write_flac_stream(const FLAC__StreamDecoder *deco
     else if (frame->header.number_type == FLAC__FRAME_NUMBER_TYPE_SAMPLE_NUMBER)
         fd->sample_pos = frame->header.number.sample_number;
 
-    if ((FLAC__uint64)(uintptr_t)voice->LoopEnd > 0 &&
-        fd->sample_pos + samples >= (FLAC__uint64)(uintptr_t)voice->LoopEnd)
+    if ((FLAC__uint64)(uintptr_t)voice->Loop.End > 0 &&
+        fd->sample_pos + samples >= (FLAC__uint64)(uintptr_t)voice->Loop.End)
     {
-        samples = (FLAC__uint64)(uintptr_t)voice->LoopEnd - fd->sample_pos;
-        if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->LoopStart))
+        samples = (FLAC__uint64)(uintptr_t)voice->Loop.End - fd->sample_pos;
+        if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->Loop.Start))
             MV_Printf("MV_GetNextFLACBlock FLAC__stream_decoder_seek_absolute: LOOP_START %ul, LOOP_END %ul\n",
-                      (FLAC__uint64)(uintptr_t)voice->LoopStart, (FLAC__uint64)(uintptr_t)voice->LoopEnd);
+                      (FLAC__uint64)(uintptr_t)voice->Loop.Start, (FLAC__uint64)(uintptr_t)voice->Loop.End);
     }
 
     size_t const size = samples * voice->channels * (voice->bits >> 3);
 
     voice->position = 0;
-    voice->BlockLength = 0;
     // CODEDUP multivoc.c MV_SetVoicePitch
     voice->RateScale = divideu32(voice->SamplingRate * voice->PitchScale, MV_MixRate);
     voice->FixedPointBufferSize = (voice->RateScale * MV_MIXBUFFERSIZE) - voice->RateScale;
@@ -246,7 +245,7 @@ FLAC__StreamDecoderWriteStatus write_flac_stream(const FLAC__StreamDecoder *deco
 
     if (size > fd->blocksize)
     {
-        block = (char *)Xmalloc(size);
+        block = (char *)Xaligned_alloc(16, size);
 
         if (block == nullptr)
             return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
@@ -279,7 +278,7 @@ FLAC__StreamDecoderWriteStatus write_flac_stream(const FLAC__StreamDecoder *deco
         char * oldblock = fd->block;
         fd->block = block;
         fd->blocksize = size;
-        Xfree(oldblock);
+        ALIGNED_FREE_AND_NULL(oldblock);
     }
 
     return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
@@ -324,10 +323,10 @@ static playbackstatus MV_GetNextFLACBlock(VoiceNode *voice)
     FLAC__StreamDecoderState decode_state;
     // FLAC__bool decode_status;
 
-    if ((FLAC__uint64)(uintptr_t)voice->LoopEnd > 0 && fd->sample_pos >= (FLAC__uint64)(uintptr_t)voice->LoopEnd)
-        if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->LoopStart))
+    if ((FLAC__uint64)(uintptr_t)voice->Loop.End > 0 && fd->sample_pos >= (FLAC__uint64)(uintptr_t)voice->Loop.End)
+        if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->Loop.Start))
             MV_Printf("MV_GetNextFLACBlock FLAC__stream_decoder_seek_absolute: LOOP_START %ul, LOOP_END %ul\n",
-                      (FLAC__uint64)(uintptr_t)voice->LoopStart, (FLAC__uint64)(uintptr_t)voice->LoopEnd);
+                      (FLAC__uint64)(uintptr_t)voice->Loop.Start, (FLAC__uint64)(uintptr_t)voice->Loop.End);
 
     /*decode_status =*/FLAC__stream_decoder_process_single(fd->stream);
     decode_state = FLAC__stream_decoder_get_state(fd->stream);
@@ -348,11 +347,11 @@ static playbackstatus MV_GetNextFLACBlock(VoiceNode *voice)
 
     if (decode_state == FLAC__STREAM_DECODER_END_OF_STREAM)
     {
-        if (voice->LoopSize > 0)
+        if (voice->Loop.Size > 0)
         {
-            if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->LoopStart))
+            if (!FLAC__stream_decoder_seek_absolute(fd->stream, (FLAC__uint64)(uintptr_t)voice->Loop.Start))
                 MV_Printf("MV_GetNextFLACBlock FLAC__stream_decoder_seek_absolute: LOOP_START %ul\n",
-                          (FLAC__uint64)(uintptr_t)voice->LoopStart);
+                          (FLAC__uint64)(uintptr_t)voice->Loop.Start);
         }
         else
             return NoMoreData;
@@ -421,27 +420,25 @@ priority.
 
 int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitchoffset, int vol, int left, int right, int priority, fix16_t volume, intptr_t callbackval)
 {
-    VoiceNode *voice;
-    flac_data *fd = 0;
-    FLAC__Metadata_Chain *metadata_chain;
-
     UNREFERENCED_PARAMETER(loopend);
 
     if (!MV_Installed)
         return MV_SetErrorCode(MV_NotInstalled);
 
-    fd = (flac_data *)Xcalloc(1, sizeof(flac_data));
-    if (!fd)
-        return MV_SetErrorCode(MV_InvalidFile);
+    // Request a voice from the voice pool
+    auto voice = MV_AllocVoice(priority, sizeof(flac_data));
+    if (voice == nullptr)
+        return MV_SetErrorCode(MV_NoVoices);
 
-    fd->ptr = ptr;
-    fd->pos = 0;
-    fd->blocksize = 0;
-    fd->length = length;
+    auto fd = (flac_data *)voice->rawdataptr;
 
-    fd->block = nullptr;
-
-    fd->stream = FLAC__stream_decoder_new();
+    fd->owner      = voice;
+    fd->ptr        = ptr;
+    fd->pos        = 0;
+    fd->blocksize  = 0;
+    fd->length     = length;
+    fd->block      = nullptr;
+    fd->stream     = FLAC__stream_decoder_new();
     fd->sample_pos = 0;
 
     FLAC__stream_decoder_set_metadata_ignore_all(fd->stream);
@@ -451,40 +448,19 @@ int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitc
                                          /*metadata_flac_stream*/ nullptr, error_flac_stream,
                                          (void *)fd) != FLAC__STREAM_DECODER_INIT_STATUS_OK)
     {
-        Xfree(fd);
         MV_Printf("MV_PlayFLAC: %s\n", FLAC__stream_decoder_get_resolved_state_string(fd->stream));
+        ALIGNED_FREE_AND_NULL(fd);
         return MV_SetErrorCode(MV_InvalidFile);
     }
 
-    // Request a voice from the voice pool
-    voice = MV_AllocVoice(priority);
-    if (voice == nullptr)
-    {
-        FLAC__stream_decoder_finish(fd->stream);
-        FLAC__stream_decoder_delete(fd->stream);
-        Xfree(fd);
-        return MV_SetErrorCode(MV_NoVoices);
-    }
-
-    fd->owner = voice;
-
     voice->wavetype = FMT_FLAC;
-    voice->rawdataptr = (void *)fd;
     voice->GetSound = MV_GetNextFLACBlock;
     voice->NextBlock = fd->block;
-    voice->LoopCount = 0;
-    voice->BlockLength = 0;
     voice->PitchScale = PITCH_GetScale(pitchoffset);
-    voice->next = nullptr;
-    voice->prev = nullptr;
     voice->priority = priority;
     voice->callbackval = callbackval;
 
-    voice->Paused = FALSE;
-
-    voice->LoopStart = 0;
-    voice->LoopEnd = 0;
-    voice->LoopSize = (loopstart >= 0 ? 1 : 0);
+    voice->Loop = { nullptr, nullptr, 0, loopstart >= 0 };
 
     // parse metadata
     // loop parsing designed with multiple repetitions in mind
@@ -492,7 +468,7 @@ int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitc
     // &tags->data.vorbis_comment)
     // but libvorbisfile may be confused by the signedness of char* vs FLAC__byte* and this code does not depend on
     // HAVE_VORBIS.
-    metadata_chain = FLAC__metadata_chain_new();
+    auto metadata_chain = FLAC__metadata_chain_new();
     if (metadata_chain != nullptr)
     {
         if (FLAC__metadata_chain_read_with_callbacks(metadata_chain, fd, flac_callbacks))
@@ -521,7 +497,7 @@ int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitc
                             // FLAC__metadata_chain_delete(metadata_chain);
                             FLAC__stream_decoder_finish(fd->stream);
                             FLAC__stream_decoder_delete(fd->stream);
-                            Xfree(fd);
+                            ALIGNED_FREE_AND_NULL(fd);
                             return MV_SetErrorCode(MV_InvalidFile);
                         }
 
@@ -577,29 +553,29 @@ int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitc
                         const FLAC__int64 flac_loopstart = atol(vc_loopstart);
                         if (flac_loopstart >= 0)  // a loop starting at 0 is valid
                         {
-                            voice->LoopStart = (const char *)(intptr_t)flac_loopstart;
-                            voice->LoopSize = 1;
+                            voice->Loop.Start = (const char *)(intptr_t)flac_loopstart;
+                            voice->Loop.Size = 1;
                         }
                     }
                     Xfree(vc_loopstart);
                 }
                 if (vc_loopend != nullptr)
                 {
-                    if (voice->LoopSize > 0)
+                    if (voice->Loop.Size > 0)
                     {
                         const FLAC__int64 flac_loopend = atol(vc_loopend);
                         if (flac_loopend > 0)  // a loop ending at 0 is invalid
-                            voice->LoopEnd = (const char *)(intptr_t)flac_loopend;
+                            voice->Loop.End = (const char *)(intptr_t)flac_loopend;
                     }
                     Xfree(vc_loopend);
                 }
                 if (vc_looplength != nullptr)
                 {
-                    if (voice->LoopSize > 0 && voice->LoopEnd == 0)
+                    if (voice->Loop.Size > 0 && voice->Loop.End == 0)
                     {
                         const FLAC__int64 flac_looplength = atol(vc_looplength);
                         if (flac_looplength > 0)  // a loop of length 0 is invalid
-                            voice->LoopEnd = (const char *)((intptr_t)flac_looplength + (intptr_t)voice->LoopStart);
+                            voice->Loop.End = (const char *)((intptr_t)flac_looplength + (intptr_t)voice->Loop.Start);
                     }
                     Xfree(vc_looplength);
                 }
@@ -632,11 +608,11 @@ int MV_PlayFLAC(char *ptr, uint32_t length, int loopstart, int loopend, int pitc
 
 void MV_ReleaseFLACVoice(VoiceNode *voice)
 {
-    if (voice->wavetype != FMT_FLAC || voice->rawdataptr == nullptr)
-        return;
+    Bassert(voice->wavetype == FMT_FLAC && voice->rawdataptr != nullptr && voice->rawdatasiz == sizeof(flac_data));
 
     flac_data *fd = (flac_data *)voice->rawdataptr;
     voice->rawdataptr = nullptr;
+    voice->rawdatasiz = 0;
 
     if (fd->stream != nullptr)
     {
@@ -650,8 +626,8 @@ void MV_ReleaseFLACVoice(VoiceNode *voice)
     auto block = fd->block;
     fd->block = nullptr;
 
-    Xfree(block);
-    Xfree(fd);
+    ALIGNED_FREE_AND_NULL(block);
+    ALIGNED_FREE_AND_NULL(fd);
 }
 #else
 #include "_multivc.h"

commit 176a7790f547d722f53f88eebb8144f9ec1134b3

audiolib: fix DemandFeed playback
Broken in 09dc0cc9489d33e86ddd5f1605014b63cb612139.

source/audiolib/src/_multivc.h
@@ -128,14 +128,13 @@ typedef struct VoiceNode
     uint32_t (*mix)(struct VoiceNode *, uint32_t);
 
     const char *sound;
+    void *rawdataptr;
 
     union
     {
-        void *rawdataptr;
+        const char *NextBlock;
         void (*DemandFeed)(const char** ptr, uint32_t* length, void* userdata);
     };
-
-    const char *NextBlock;
 
     struct
     {

source/audiolib/src/multivoc.cpp
@@ -1031,7 +1031,6 @@ int MV_StartDemandFeedPlayback(void (*function)(const char** ptr, uint32_t* leng
     voice->bits = bitdepth;
     voice->channels = channels;
     voice->GetSound = MV_GetNextDemandFeedBlock;
-    voice->NextBlock  = nullptr;
     voice->DemandFeed = function;
     voice->position = 0;
     voice->sound  = nullptr;