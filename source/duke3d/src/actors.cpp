nukeykt/NBlood

commit 6335a9a2e515329a159b03ba0c8a32136dc6ac42

Duke3d: add player .somethingonplayer check to GREENSLIME, and reset the player's .somethingonplayer to -1 if the player is dead

source/duke3d/src/actors.cpp
@@ -4740,10 +4740,11 @@ ACTOR_STATIC void G_MoveActors(void)
 
             pSprite->cstat = (playerDist < 1596) ? 0 : 257;
 
-            if (pData[0] == -4) //On the player
+            if (pData[0] == -4 && pPlayer->somethingonplayer == spriteNum) //On the player
             {
                 if (sprite[pPlayer->i].extra < 1)
                 {
+                    pPlayer->somethingonplayer = -1;
                     pData[0] = 0;
                     goto next_sprite;
                 }

commit e3946bf81fe328602506f74c17f5224e20e53db0

Duke3d: fix GREENSLIME issue introduced by 6335a9a2e515329a159b03ba0c8a32136dc6ac42

source/duke3d/src/actors.cpp
@@ -4754,12 +4754,12 @@ ACTOR_STATIC void G_MoveActors(void)
 
             pSprite->cstat = (playerDist < 1596) ? 0 : 257;
 
-            if (pData[0] == GREENSLIME_ONPLAYER && pPlayer->somethingonplayer == spriteNum)
+            if (pData[0] == GREENSLIME_ONPLAYER)
             {
-                if (sprite[pPlayer->i].extra < 1)
+                if (sprite[pPlayer->i].extra < 1 && pPlayer->somethingonplayer == spriteNum)
                 {
                     pPlayer->somethingonplayer = -1;
-                    pData[0] = GREENSLIME_DONEEATING;
+                    pData[0] = GREENSLIME_TOFLOOR;
                     goto next_sprite;
                 }
 
@@ -4842,10 +4842,9 @@ ACTOR_STATIC void G_MoveActors(void)
 
                 goto next_sprite;
             }
-
             else if (pSprite->xvel < 64 && playerDist < 768)
             {
-                if (pPlayer->somethingonplayer == -1)
+                if (pPlayer->somethingonplayer == -1 && sprite[pPlayer->i].extra > 0)
                 {
                     pPlayer->somethingonplayer = spriteNum;
 