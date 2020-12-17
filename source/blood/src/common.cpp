cbd500fe6c4a389a92e13d114c03d7a8032d52c2
sirlemonhead <sirlemonhead@outlook.com>
2020-12-17 23:23:39 +0000

    NBlood: Add GOG.com One Unit Whole Blood detection for newer installer/Galaxy install

--- source/blood/src/common.cpp
+++ source/blood/src/common.cpp
@@ -360,6 +360,14 @@ void G_AddSearchPaths(void)
         found = true;
     }
 
+    // Blood: One Unit Whole Blood - GOG.com (new installer/GOG Galaxy)
+    bufsize = sizeof(buf);
+    if (!found && Paths_ReadRegistryValue(R"(SOFTWARE\GOG.com\Games\1207658856)", "path", buf, &bufsize))
+    {
+        addsearchpath(buf);
+        found = true;
+    }
+
     // Blood: One Unit Whole Blood - GOG.com
     bufsize = sizeof(buf);
     if (!found && Paths_ReadRegistryValue("SOFTWARE\\GOG.com\\GOGONEUNITONEBLOOD", "PATH", buf, &bufsize))
