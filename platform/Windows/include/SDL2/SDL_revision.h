a57a6e1a5b7a4bb80d7a00d73fa4c3f431256382
Richard C. Gobeille <richard@voidpoint.com>
2021-06-23 23:06:43 -0700

    engine: update to SDL 814285d6 on Windows

--- platform/Windows/include/SDL2/SDL_revision.h
+++ platform/Windows/include/SDL2/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@db5cd8c694086c4bd54bee52cec48e4b8907fd51"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@814285d6e30d1a84e6850038bc1fcc6b67ef4f35"
 #define SDL_REVISION_NUMBER 0
