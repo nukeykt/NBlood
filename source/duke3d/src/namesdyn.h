cd768b4ed0187419604e3eec9655747906666887
Richard C. Gobeille <richard@voidpoint.com>
2021-06-29 16:41:55 -0700

    Duke3d: uncomment DYNTILEREMAP_ENABLE, oops

--- source/duke3d/src/namesdyn.h
+++ source/duke3d/src/namesdyn.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #define namesdyn_h__
 
 
-// #define DYNTILEREMAP_ENABLE
+#define DYNTILEREMAP_ENABLE
 
 
 #define SECTOREFFECTOR__      1
