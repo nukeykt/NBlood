6220750116c17662ee6e487d29f8b938123d1a50
Richard Gobeille <richard@voidpoint.com>
2021-07-22 10:50:08 -0700

    engine: fix copy/paste error in 24e5d5fc15bde29de7d6ced1a919dc76f6a8987e

--- source/build/src/clip.cpp
+++ source/build/src/clip.cpp
@@ -1573,7 +1573,7 @@ int32_t clipmove(vec3_t * const pos, int16_t * const sectnum, int32_t xvect, int
                 if ((tempint ^ tempint2) < 0)
                 {
                     if (enginecompatibilitymode == ENGINE_EDUKE32)
-                        clipupdatesector(vec, sectnum, rad);
+                        clipupdatesector(pos->vec2, sectnum, rad);
                     else if (enginecompatibilitymode == ENGINE_19961112)
                         updatesector(pos->x, pos->y, sectnum);
 
