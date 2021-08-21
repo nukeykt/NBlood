nukeykt/NBlood

commit 3cbb6adfba8b86e8b33b7c82f510c39e5d7e04f4

audiolib: #if 0 out some dead code in XA and XMP support
This doesn't look like it's intended to be dead code, but nothing sets Loop.Size anywhere.

source/audiolib/src/xa.cpp
@@ -348,12 +348,14 @@ static playbackstatus MV_GetNextXABlock
 
     if (xad->length == xad->pos)
     {
+#if 0
         if (voice->Loop.Size > 0)
         {
             xad->pos = XA_DATA_START;
             xad->t1 = xad->t2 = xad->t1_x = xad->t2_x = 0;
         }
         else
+#endif
             return NoMoreData;
     }
 

source/audiolib/src/xmp.cpp
@@ -26,6 +26,7 @@ static playbackstatus MV_GetNextXMPBlock(VoiceNode *voice)
 
     if (xmp_play_frame(ctx) != 0)
     {
+#if 0
         if (voice->Loop.Size > 0)
         {
             xmp_restart_module(ctx);
@@ -33,6 +34,7 @@ static playbackstatus MV_GetNextXMPBlock(VoiceNode *voice)
                 return NoMoreData;
         }
         else
+#endif
             return NoMoreData;
     }
 

commit b059713b033d0deb686773a03ad8b7b4a527f3ce

audiolib: this probably fixes XA looping, broken by me in 3cbb6adf

source/audiolib/src/xa.cpp
@@ -348,14 +348,12 @@ static playbackstatus MV_GetNextXABlock
 
     if (xad->length == xad->pos)
     {
-#if 0
         if (voice->Loop.Size > 0)
         {
             xad->pos = XA_DATA_START;
             xad->t1 = xad->t2 = xad->t1_x = xad->t2_x = 0;
         }
         else
-#endif
             return NoMoreData;
     }
 