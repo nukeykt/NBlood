93fb2f60baffff2d56e3b28be1f2caa93c24c1e7
carnivoroussociety <38839485+carnivoroussociety@users.noreply.github.com>
2021-08-21 20:53:20 +1000

    Renamed SpidPoisonTarget to SpidPoisonPlayer

--- source/blood/src/aispid.cpp
+++ source/blood/src/aispid.cpp
@@ -60,7 +60,7 @@ AISTATE spidBite = { kAiStateChase, 6, nBiteClient, 60, NULL, NULL, NULL, &spidC
 AISTATE spidJump = { kAiStateChase, 8, nJumpClient, 60, NULL, aiMoveForward, NULL, &spidChase };
 AISTATE spidBirth = { kAiStateOther, 0, nBirthClient, 60, NULL, NULL, NULL, &spidIdle };
 
-static char SpidPoisonTarget(XSPRITE *pXDude, int nBlind, int max)
+static char SpidPoisonPlayer(XSPRITE *pXDude, int nBlind, int max)
 {
     dassert(pXDude != NULL);
     int nDude = pXDude->reference;
@@ -107,11 +107,11 @@ static void SpidBiteSeqCallback(int, int nXSprite)
                     break;
                 case kDudeSpiderRed:
                     actFireVector(pSprite, 0, 0, dx, dy, dz, kVectorSpiderBite);
-                    if (Chance(0x5000)) SpidPoisonTarget(pXTarget, 4, 16);
+                    if (Chance(0x5000)) SpidPoisonPlayer(pXTarget, 4, 16);
                     break;
                 case kDudeSpiderBlack:
                     actFireVector(pSprite, 0, 0, dx, dy, dz, kVectorSpiderBite);
-                    SpidPoisonTarget(pXTarget, 8, 16);
+                    SpidPoisonPlayer(pXTarget, 8, 16);
                     break;
                 case kDudeSpiderMother: {
                     actFireVector(pSprite, 0, 0, dx, dy, dz, kVectorSpiderBite);
@@ -120,7 +120,7 @@ static void SpidBiteSeqCallback(int, int nXSprite)
                     dy += Random2(2000);
                     dz += Random2(2000);
                     actFireVector(pSprite, 0, 0, dx, dy, dz, kVectorSpiderBite);
-                    SpidPoisonTarget(pXTarget, 8, 16);
+                    SpidPoisonPlayer(pXTarget, 8, 16);
                     break;
                 }
             }
