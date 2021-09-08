0fb5f94d8232cfbb7b2600b8862133c0fe81f627
Richard Gobeille <richard@voidpoint.com>
2021-09-07 23:11:53 -0700

    engine: update libdivide to b322221677351ebb11f0a42fe9a9a2794da5bfe5, add libdivide_config.h
    engine: update libdivide to b322221677351ebb11f0a42fe9a9a2794da5bfe5, add libdivide_config.h

--- source/build/include/compat.h
+++ source/build/include/compat.h
@@ -1433,14 +1433,8 @@ static inline void maybe_grow_buffer(char ** const buffer, int32_t * const buffe
 
 ////////// Inlined external libraries //////////
 
-#ifndef LIBDIVIDE_BODY
-# define LIBDIVIDE_HEADER_ONLY
-#endif
-#define LIBDIVIDE_C_HEADERS
-#define LIBDIVIDE_NONAMESPACE
-#define LIBDIVIDE_NOINLINE
+#include "libdivide_config.h"
 #include "fix16.h"
-#include "libdivide.h"
 
 #ifdef __cplusplus
 #include "clockticks.hpp"
