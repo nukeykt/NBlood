1bdadc4d837136da35a81accee7ce629d99eef63
carnivoroussociety <38839485+carnivoroussociety@users.noreply.github.com>
2021-09-14 23:34:57 +1000

    Don't render shadows for rooms with ror floors
    This reverts attempt to render shadows through ror, which caused others various issues with levels

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -2316,23 +2316,16 @@ tspritetype *viewAddEffect(int nTSprite, VIEW_EFFECT nViewEffect)
     }
     case kViewEffectShadow:
     {
+        if (!VanillaMode()) // if floor has ror, don't render effect
+        {
+            sectortype *pSector = &sector[pTSprite->sectnum];
+            if ((pSector->floorpicnum >= 4080) && (pSector->floorpicnum <= 4095))
+                break;
+        }
         auto pNSprite = viewInsertTSprite(pTSprite->sectnum, 32767, pTSprite);
         if (!pNSprite)
             break;
         pNSprite->z = getflorzofslope(pTSprite->sectnum, pNSprite->x, pNSprite->y);
-        if ((sector[pNSprite->sectnum].floorpicnum >= 4080) && (sector[pNSprite->sectnum].floorpicnum <= 4095) && !VanillaMode()) // if floor has ror, find actual floor
-        {
-            int cX = pNSprite->x, cY = pNSprite->y, cZ = pNSprite->z, nSectnum = pNSprite->sectnum;
-            for (int i = 0; i < 16; i++) // scan through max stacked sectors
-            {
-                if (!CheckLink(&cX, &cY, &cZ, &nSectnum)) // if no more floors underneath, abort
-                    break;
-                cZ = getflorzofslope(nSectnum, cX, cY);
-                if ((sector[nSectnum].floorpicnum < 4080) || (sector[nSectnum].floorpicnum > 4095)) // if current sector is not open air, use as floor for shadow casting, otherwise continue to next sector
-                    break;
-            }
-            pNSprite->x = cX, pNSprite->y = cY, pNSprite->z = cZ, pNSprite->sectnum = nSectnum;
-        }
         pNSprite->shade = 127;
         pNSprite->cstat |= 2;
         pNSprite->xrepeat = pTSprite->xrepeat;
@@ -2451,7 +2444,7 @@ tspritetype *viewAddEffect(int nTSprite, VIEW_EFFECT nViewEffect)
             pNSprite->picnum = nVoxel;
             if (pPlayer->curWeapon == 9) // position lifeleech behind player
             {
-                pNSprite->x +=  mulscale30(128, Cos(gView->pSprite->ang));
+                pNSprite->x += mulscale30(128, Cos(gView->pSprite->ang));
                 pNSprite->y += mulscale30(128, Sin(gView->pSprite->ang));
             }
             if ((pPlayer->curWeapon == 9) || (pPlayer->curWeapon == 10))  // make lifeleech/voodoo doll always face viewer like sprite
