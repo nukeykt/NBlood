e9eeca6b9a3c8f61f7864cbce5ca7b19b88916e5
NY00123 <ny00@outlook.com>
2022-01-15 22:05:31 +0200

    RR/Blood/Exhumed: Implement startwin_isopen

--- source/blood/src/startgtk.game.cpp
+++ source/blood/src/startgtk.game.cpp
@@ -618,6 +618,10 @@ static GtkWidget *create_window(void)
 
 
 // -- BUILD ENTRY POINTS ------------------------------------------------------
+bool startwin_isopen(void)
+{
+    return gtkenabled && stwidgets.startwin;
+}
 
 int32_t startwin_open(void)
 {
