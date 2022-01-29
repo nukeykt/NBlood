c9ca196acacfa394779a6f85fe3651dd618b5362
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-01-29 18:18:06 +1000

    Rename sub_746D4() -> fxSpawnPodBlood()
    Also rename CDemo's at59ef to nDemosFound

--- source/blood/src/demo.h
+++ source/blood/src/demo.h
@@ -104,7 +104,7 @@ public:
     const char **pzDemoFile;
     DEMOCHAIN *pFirstDemo;
     DEMOCHAIN *pCurrentDemo;
-    int at59ef;
+    int nDemosFound;
 };
 
 extern CDemo gDemo;
--- source/blood/src/fx.cpp
+++ source/blood/src/fx.cpp
@@ -289,7 +289,7 @@ void fxSpawnBlood(spritetype *pSprite, int a2)
     }
 }
 
-void sub_746D4(spritetype *pSprite, int a2)
+void fxSpawnPodBlood(spritetype *pSprite, int a2)
 {
     UNREFERENCED_PARAMETER(a2);
     if (pSprite->sectnum < 0 || pSprite->sectnum >= numsectors)
