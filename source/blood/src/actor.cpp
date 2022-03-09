3fefdfa85bb86b5e8e951e886a6d13c769666215
‮ <38839485+carnivoroussociety@users.noreply.github.com>
2022-03-09 15:10:50 +1000

    Set inittype on actSpawnDude()

--- source/blood/src/actor.cpp
+++ source/blood/src/actor.cpp
@@ -6167,6 +6167,8 @@ spritetype *actSpawnDude(spritetype *pSource, short nType, int a3, int a4)
         y = pSource->y+mulscale30r(Sin(angle), a3);
     }
     pSprite2->type = nType;
+    if (!VanillaMode())
+        pSprite2->inittype = nType;
     pSprite2->ang = angle;
     vec3_t pos = { x, y, z };
     setsprite(pSprite2->index, &pos);
