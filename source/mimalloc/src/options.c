2cb3ab4eac09a0cd97a6a45c7a8f07feeb4d13e3
Richard Gobeille <richard@voidpoint.com>
2022-05-02 23:28:53 -0700

    engine: enable large page support in mimalloc
    I think changing the default value here is the only way to ensure the option is set at process initialization time, before control is passed to main(). It looks like the "official" way to enable this is to set some environmental variables before spawning the process, but I don't think that solution is really appropriate for production builds.

--- source/mimalloc/src/options.c
+++ source/mimalloc/src/options.c
@@ -74,7 +74,7 @@ static mi_option_desc_t options[_mi_option_last] =
   { 1, UNINIT, MI_OPTION(eager_region_commit) },
   { 0, UNINIT, MI_OPTION(reset_decommits) },     // reset uses MADV_FREE/MADV_DONTNEED
   #endif
-  { 0, UNINIT, MI_OPTION(large_os_pages) },      // use large OS pages, use only with eager commit to prevent fragmentation of VMA's
+  { 1, UNINIT, MI_OPTION(large_os_pages) },      // use large OS pages, use only with eager commit to prevent fragmentation of VMA's
   { 0, UNINIT, MI_OPTION(reserve_huge_os_pages) },  // per 1GiB huge pages
   { -1, UNINIT, MI_OPTION(reserve_huge_os_pages_at) }, // reserve huge pages at node N
   { 0, UNINIT, MI_OPTION(reserve_os_memory)     },
