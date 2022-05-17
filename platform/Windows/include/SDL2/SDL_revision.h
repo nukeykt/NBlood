70d4d6f6b45ec115843eb23d373555101b7f60ec
Richard Gobeille <richard@voidpoint.com>
2022-05-17 08:56:11 -0700

    engine: update SDL to 2.0.22 on Windows

--- platform/Windows/include/SDL2/SDL_revision.h
+++ platform/Windows/include/SDL2/SDL_revision.h
@@ -1,2 +1,2 @@
-#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@b424665e0899769b200231ba943353a5fee1b6b6"
+#define SDL_REVISION "https://github.com/libsdl-org/SDL.git@f8e14ad25aa6f0b4bbed411583a9863c855ad9e6"
 #define SDL_REVISION_NUMBER 0
