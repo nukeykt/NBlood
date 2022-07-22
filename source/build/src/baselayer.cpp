b94a9eb7303913c07c55e88465ed2e175f52eb25
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2022-07-22 08:02:23 -0500

    initprintf also log to nblood.log (issue nukeykt#610)

--- source/build/src/baselayer.cpp
+++ source/build/src/baselayer.cpp
@@ -70,7 +70,7 @@ int initprintf(const char *f, ...)
     } while ((unsigned)len >= size);
 
     initputs(buf);
-    LOG_F(INFO, buf); // also log to nblood.log
+    LOG_F(INFO, buf); // also write to the .log file
     Xfree(buf);
 
     return len;
