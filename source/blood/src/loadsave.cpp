b92b744b0e98c2adfd893081d8a4662d50e36efe
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-10-28 16:34:49 +1000

    Refresh view size on load game

--- source/blood/src/loadsave.cpp
+++ source/blood/src/loadsave.cpp
@@ -160,6 +160,7 @@ void LoadSave::LoadGame(char *pzFile)
     else
         gGameMessageMgr.Clear();
     viewSetErrorMessage("");
+    viewResizeView(gViewSize);
     if (!gGameStarted)
     {
         netWaitForEveryone(0);
