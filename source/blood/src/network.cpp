d2be06469efc68b92d07d938a835f3cec57d4229
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2022-10-31 20:54:39 +1000

    Rename word_1328AC to kNetVersion

--- source/blood/src/network.cpp
+++ source/blood/src/network.cpp
@@ -67,7 +67,7 @@ char gNetAddress[32];
 // PORT-TODO: Use different port?
 int gNetPort = kNetDefaultPort;
 
-const short word_1328AC = 0x214;
+const short kNetVersion = 0x214;
 
 PKT_STARTGAME gPacketStartGame;
 
@@ -505,7 +505,7 @@ void netGetPackets(void)
         case 252:
             pPacket += 4;
             memcpy(&gPacketStartGame, pPacket, sizeof(PKT_STARTGAME));
-            if (gPacketStartGame.version != word_1328AC)
+            if (gPacketStartGame.version != kNetVersion)
                 ThrowError("\nThese versions of Blood cannot play together.\n");
             gStartNewGame = 1;
             break;
@@ -562,7 +562,7 @@ void netBroadcastNewGame(void)
 {
     if (numplayers < 2)
         return;
-    gPacketStartGame.version = word_1328AC;
+    gPacketStartGame.version = kNetVersion;
     char *pPacket = packet;
     PutPacketByte(pPacket, 252);
     PutPacketDWord(pPacket, myconnectindex);
