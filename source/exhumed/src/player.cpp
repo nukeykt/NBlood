372cab1596e81c2d6db92141e50cc59307e7a3c4
sirlemonhead <sirlemonhead@outlook.com>
2023-04-03 19:11:29 +0100

    PCExhumed: Fix not setting old sector value in player code. Fixes #704

--- source/exhumed/src/player.cpp
+++ source/exhumed/src/player.cpp
@@ -2325,30 +2325,30 @@ loc_1AB8E:
                 {
                     if ((nMove & 0xC000) == 0x8000)
                     {
-                        int var_C4 = sprite[nPlayerSprite].x;
-                        int var_D4 = sprite[nPlayerSprite].y;
-                        int var_C8 = sprite[nPlayerSprite].z;
+                        int prevX = sprite[nPlayerSprite].x;
+                        int prevY = sprite[nPlayerSprite].y;
+                        int prevZ = sprite[nPlayerSprite].z;
+                        int prevSect = sprite[nPlayerSprite].sectnum;
 
                         mychangespritesect(nPlayerSprite, nViewSect);
 
                         sprite[nPlayerSprite].x = spr_x;
                         sprite[nPlayerSprite].y = spr_y;
 
-                        int var_FC = sector[nViewSect].floorz + (-5120);
-
-                        sprite[nPlayerSprite].z = var_FC;
+                        int newZ = sector[nViewSect].floorz + (-5120);
+                        sprite[nPlayerSprite].z = newZ;
 
                         if ((movesprite(nPlayerSprite, x, y, 0, 5120, 0, CLIPMASK0) & 0xC000) == 0x8000)
                         {
-                            mychangespritesect(nPlayerSprite, sprite[nPlayerSprite].sectnum);
+                            mychangespritesect(nPlayerSprite, prevSect);
 
-                            sprite[nPlayerSprite].x = var_C4;
-                            sprite[nPlayerSprite].y = var_D4;
-                            sprite[nPlayerSprite].z = var_C8;
+                            sprite[nPlayerSprite].x = prevX;
+                            sprite[nPlayerSprite].y = prevY;
+                            sprite[nPlayerSprite].z = prevZ;
                         }
                         else
                         {
-                            sprite[nPlayerSprite].z = var_FC - 256;
+                            sprite[nPlayerSprite].z = newZ - 256;
                             D3PlayFX(StaticSound[kSoundJonWade], nPlayerSprite);
                         }
                     }
