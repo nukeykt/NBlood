ceb67ff6bdeef7afcb795d200a423c538f611ef7
Richard Gobeille <richard@voidpoint.com>
2023-05-25 03:04:09 -0700

    Duke3d: add console error spew for a couple conditions which can cause creating a savegame to fail to capture a meaningful screenshot

--- source/duke3d/src/game.cpp
+++ source/duke3d/src/game.cpp
@@ -804,7 +804,11 @@ void G_DrawRooms(int32_t playerNum, int32_t smoothRatio)
     VM_OnEvent(EVENT_DISPLAYSTART, pPlayer->i, playerNum);
 
     if ((ud.overhead_on == 2 && !automapping) || ud.show_help || (pPlayer->cursectnum == -1 && videoGetRenderMode() != REND_CLASSIC))
+    {
+        if (g_screenCapture && pPlayer->cursectnum == -1)
+            LOG_F(ERROR, "Unable to capture screenshot for savegame because player cursectnum is -1!");
         return;
+    }
 
     if (r_usenewaspect)
     {
@@ -824,6 +828,17 @@ void G_DrawRooms(int32_t playerNum, int32_t smoothRatio)
     G_AnimateCamSprite(smoothRatio);
     G_DoConveyorInterp(smoothRatio);
     G_InterpolateLights(smoothRatio);
+
+    if (g_screenCapture)
+    {
+        walock[TILE_SAVESHOT] = CACHE1D_PERMANENT;
+
+        if (waloff[TILE_SAVESHOT] == 0)
+            g_cache.allocateBlock(&waloff[TILE_SAVESHOT],200*320,&walock[TILE_SAVESHOT]);
+
+        if (videoGetRenderMode() == REND_CLASSIC)
+            renderSetTarget(TILE_SAVESHOT, 200, 320);
+    }
 
     if (ud.camerasprite >= 0)
     {
@@ -882,114 +897,107 @@ void G_DrawRooms(int32_t playerNum, int32_t smoothRatio)
         else
             renderSetAspect(mulscale16(vr, viewingrange), yxaspect);
 
-        if (g_screenCapture)
+        if (!g_screenCapture)
         {
-            walock[TILE_SAVESHOT] = CACHE1D_PERMANENT;
-
-            if (waloff[TILE_SAVESHOT] == 0)
-                g_cache.allocateBlock(&waloff[TILE_SAVESHOT],200*320,&walock[TILE_SAVESHOT]);
-
-            if (videoGetRenderMode() == REND_CLASSIC)
-                renderSetTarget(TILE_SAVESHOT, 200, 320);
-        }
-        else if (screenTilting)
-        {
-            int32_t oviewingrange = viewingrange;  // save it from renderSetAspect()
-            const int16_t tang = (ud.screen_tilting) ? pPlayer->rotscrnang : 0;
-
-            if (tang == 1024)
-                screenTilting = 2;
-            else
+            if (screenTilting)
             {
-                // Maximum possible allocation size passed to allocache() below
-                // since there is no equivalent of free() for allocache().
-#if MAXYDIM >= 640
-                int const maxTiltSize = 640*640;
-#else
-                int const maxTiltSize = 320*320;
-#endif
-                // To render a tilted screen in high quality, we need at least
-                // 640 pixels of *Y* dimension.
-#if MAXYDIM >= 640
-                // We also need
-                //  * xdim >= 640 since tiltcx will be passed as setview()'s x2
-                //    which must be less than xdim.
-                //  * ydim >= 640 (sic!) since the tile-to-draw-to will be set
-                //    up with dimension 400x640, but the engine's arrays like
-                //    lastx[] are alloc'd with *xdim* elements! (This point is
-                //    the dynamic counterpart of the #if above since we now
-                //    allocate these engine arrays tightly.)
-                // XXX: The engine should be in charge of setting up everything
-                // so that no oob access occur.
-                if (xdim >= 640 && ydim >= 640)
+                int32_t oviewingrange = viewingrange;  // save it from renderSetAspect()
+                const int16_t tang = (ud.screen_tilting) ? pPlayer->rotscrnang : 0;
+
+                if (tang == 1024)
+                    screenTilting = 2;
+                else
                 {
-                    tiltcs = 2;
-                    tiltcx = 640;
-                    tiltcy = 400;
+                    // Maximum possible allocation size passed to allocache() below
+                    // since there is no equivalent of free() for allocache().
+    #if MAXYDIM >= 640
+                    int const maxTiltSize = 640*640;
+    #else
+                    int const maxTiltSize = 320*320;
+    #endif
+                    // To render a tilted screen in high quality, we need at least
+                    // 640 pixels of *Y* dimension.
+    #if MAXYDIM >= 640
+                    // We also need
+                    //  * xdim >= 640 since tiltcx will be passed as setview()'s x2
+                    //    which must be less than xdim.
+                    //  * ydim >= 640 (sic!) since the tile-to-draw-to will be set
+                    //    up with dimension 400x640, but the engine's arrays like
+                    //    lastx[] are alloc'd with *xdim* elements! (This point is
+                    //    the dynamic counterpart of the #if above since we now
+                    //    allocate these engine arrays tightly.)
+                    // XXX: The engine should be in charge of setting up everything
+                    // so that no oob access occur.
+                    if (xdim >= 640 && ydim >= 640)
+                    {
+                        tiltcs = 2;
+                        tiltcx = 640;
+                        tiltcy = 400;
+                    }
+                    else
+    #endif
+                    {
+                        // JBF 20030807: Increased tilted-screen quality
+                        tiltcs = 1;
+
+                        // NOTE: The same reflections as above apply here, too.
+                        // TILT_SETVIEWTOTILE_320.
+                        tiltcx = 320;
+                        tiltcy = 200;
+                    }
+
+                    // If the view is rotated (not 0 or 180 degrees modulo 360 degrees),
+                    // we render onto a square tile and display a portion of that
+                    // rotated on-screen later on.
+                    const int32_t viewtilexsiz = (tang&1023) ? tiltcx : tiltcy;
+                    const int32_t viewtileysiz = tiltcx;
+
+                    walock[TILE_TILT] = CACHE1D_PERMANENT;
+                    if (waloff[TILE_TILT] == 0)
+                        g_cache.allocateBlock(&waloff[TILE_TILT], maxTiltSize, &walock[TILE_TILT]);
+
+                    renderSetTarget(TILE_TILT, viewtilexsiz, viewtileysiz);
+
+                    if ((tang&1023) == 512)
+                    {
+                        //Block off unscreen section of 90ø tilted screen
+                        int const j = tiltcx-(60*tiltcs);
+                        for (bssize_t i=(60*tiltcs)-1; i>=0; i--)
+                        {
+                            startumost[i] = 1;
+                            startumost[i+j] = 1;
+                            startdmost[i] = 0;
+                            startdmost[i+j] = 0;
+                        }
+                    }
+
+                    int vRange = (tang & 511);
+
+                    if (vRange > 256)
+                        vRange = 512 - vRange;
+
+                    vRange = sintable[vRange + 512] * 8 + sintable[vRange] * 5;
+                    renderSetAspect(mulscale16(oviewingrange, vRange >> 1), yxaspect);
+                }
+            }
+    #ifdef USE_OPENGL
+            else if (videoGetRenderMode() >= REND_POLYMOST)
+            {
+                if (ud.screen_tilting
+    #ifdef SPLITSCREEN_MOD_HACKS
+                    && !g_fakeMultiMode
+    #endif
+                )
+                {
+                    renderSetRollAngle(pPlayer->orotscrnang + mulscale16(((pPlayer->rotscrnang - pPlayer->orotscrnang + 1024)&2047)-1024, smoothRatio));
                 }
                 else
-#endif
                 {
-                    // JBF 20030807: Increased tilted-screen quality
-                    tiltcs = 1;
-
-                    // NOTE: The same reflections as above apply here, too.
-                    // TILT_SETVIEWTOTILE_320.
-                    tiltcx = 320;
-                    tiltcy = 200;
+                    renderSetRollAngle(0);
                 }
-
-                // If the view is rotated (not 0 or 180 degrees modulo 360 degrees),
-                // we render onto a square tile and display a portion of that
-                // rotated on-screen later on.
-                const int32_t viewtilexsiz = (tang&1023) ? tiltcx : tiltcy;
-                const int32_t viewtileysiz = tiltcx;
-
-                walock[TILE_TILT] = CACHE1D_PERMANENT;
-                if (waloff[TILE_TILT] == 0)
-                    g_cache.allocateBlock(&waloff[TILE_TILT], maxTiltSize, &walock[TILE_TILT]);
-
-                renderSetTarget(TILE_TILT, viewtilexsiz, viewtileysiz);
-
-                if ((tang&1023) == 512)
-                {
-                    //Block off unscreen section of 90ø tilted screen
-                    int const j = tiltcx-(60*tiltcs);
-                    for (bssize_t i=(60*tiltcs)-1; i>=0; i--)
-                    {
-                        startumost[i] = 1;
-                        startumost[i+j] = 1;
-                        startdmost[i] = 0;
-                        startdmost[i+j] = 0;
-                    }
-                }
-
-                int vRange = (tang & 511);
-
-                if (vRange > 256)
-                    vRange = 512 - vRange;
-
-                vRange = sintable[vRange + 512] * 8 + sintable[vRange] * 5;
-                renderSetAspect(mulscale16(oviewingrange, vRange >> 1), yxaspect);
             }
+    #endif
         }
-#ifdef USE_OPENGL
-        else if (videoGetRenderMode() >= REND_POLYMOST)
-        {
-            if (ud.screen_tilting
-#ifdef SPLITSCREEN_MOD_HACKS
-                && !g_fakeMultiMode
-#endif
-            )
-            {
-                renderSetRollAngle(pPlayer->orotscrnang + mulscale16(((pPlayer->rotscrnang - pPlayer->orotscrnang + 1024)&2047)-1024, smoothRatio));
-            }
-            else
-            {
-                renderSetRollAngle(0);
-            }
-        }
-#endif
 
         if (pPlayer->newowner < 0)
         {
@@ -1150,6 +1158,9 @@ void G_DrawRooms(int32_t playerNum, int32_t smoothRatio)
 
         if (g_screenCapture)
         {
+            if (noDraw)
+                LOG_F(ERROR, "Unable to capture screenshot for savegame because EVENT_DISPLAYROOMS returned %d!", noDraw);
+
             g_screenCapture = 0;
 
             if (videoGetRenderMode() == REND_CLASSIC)
