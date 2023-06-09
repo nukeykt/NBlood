2a3439d78e058a9f9cf08d6706deeefb0e9e9ed4
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2023-06-10 06:33:21 +1000

    Adjust show weapon position for voxels

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -1246,6 +1246,23 @@ struct WEAPONICON {
 WEAPONICON gWeaponIcon[] = {
     { -1, 0 },
     { -1, 0 }, // 1: pitchfork
+    { 524, 4 }, // 2: flare gun
+    { 559, 7 }, // 3: shotgun
+    { 558, 5 }, // 4: tommy gun
+    { 526, 11 }, // 5: napalm launcher
+    { 589, 7 }, // 6: dynamite
+    { 618, 6 }, // 7: spray can
+    { 539, 11 }, // 8: tesla gun
+    { 800, 0 }, // 9: life leech
+    { 525, 13 }, // 10: voodoo doll
+    { 811, 7 }, // 11: proxy bomb
+    { 810, 7 }, // 12: remote bomb
+    { -1, 0 },
+};
+
+WEAPONICON gWeaponIconVoxel[] = {
+    { -1, 0 },
+    { -1, 0 }, // 1: pitchfork
     { 524, 6 }, // 2: flare gun
     { 559, 6 }, // 3: shotgun
     { 558, 8 }, // 4: tommy gun
@@ -2454,12 +2471,13 @@ tspritetype *viewAddEffect(int nTSprite, VIEW_EFFECT nViewEffect)
         pNSprite->x = pTSprite->x;
         pNSprite->y = pTSprite->y;
         pNSprite->z = pTSprite->z-(32<<8);
-        pNSprite->z -= weaponIcon.zOffset<<8; // offset up
         pNSprite->picnum = nTile;
         pNSprite->shade = pTSprite->shade;
         pNSprite->xrepeat = 32;
         pNSprite->yrepeat = 32;
         pNSprite->ang = (gCameraAng + kAng90) & kAngMask; // always face viewer
+        if (VanillaMode())
+            break;
         const int nVoxel = voxelIndex[nTile];
         if (gShowWeapon == 2 && usevoxels && gDetail >= 4 && videoGetRenderMode() != REND_POLYMER && nVoxel != -1)
         {
@@ -2475,7 +2493,10 @@ tspritetype *viewAddEffect(int nTSprite, VIEW_EFFECT nViewEffect)
                 pNSprite->ang = (gCameraAng + kAng180) & kAngMask;
             else if ((pPlayer->curWeapon == kWeaponProxyTNT) || (pPlayer->curWeapon == kWeaponRemoteTNT)) // make proxy/remote tnt always face viewers like sprite
                 pNSprite->ang = (gCameraAng + kAng180 + kAng45) & kAngMask;
+            pNSprite->z -= gWeaponIconVoxel[pPlayer->curWeapon].zOffset<<8; // offset up
         }
+        else
+            pNSprite->z -= weaponIcon.zOffset<<8; // offset up
         break;
     }
     }
