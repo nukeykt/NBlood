e43a642fc4716049e38e67d8b6e86b4cede4d4bb
Evan Ramos <hendricks266@gmail.com>
2023-06-09 04:24:43 -0500

    Use std::numeric_limits instead of cfloat's FLT_MAX and DBL_MAX
    Applies a545fe8b74697926825ba5420ae49caf35f56cf7 to RR and Blood
    
    Replaces 0a1dec70ac49a8fb217647214fb22d0c8b725a78

--- source/blood/src/view.cpp
+++ source/blood/src/view.cpp
@@ -23,8 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <stdlib.h>
 #include <string.h>
 
-#include <cfloat>
-
 #include "compat.h"
 #include "a.h"
 #include "build.h"
@@ -4113,8 +4111,8 @@ void viewPrintFPS(void)
     static int32_t frameCount;
     static double cumulativeFrameDelay;
     static double lastFrameTime;
-    static float lastFPS, minFPS = FLT_MAX, maxFPS;
-    static double minGameUpdate = DBL_MAX, maxGameUpdate;
+    static float lastFPS, minFPS = std::numeric_limits<float>::max(), maxFPS;
+    static double minGameUpdate = std::numeric_limits<double>::max(), maxGameUpdate;
 
     double frameTime = timerGetFractionalTicks();
     double frameDelay = frameTime - lastFrameTime;
