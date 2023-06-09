116f54519fca3303ba89823bda07e8bb4f371e2b
‮ <80724828+tmyqlfpir@users.noreply.github.com>
2023-06-10 06:17:43 +1000

    Set vsync at start

--- source/blood/src/blood.cpp
+++ source/blood/src/blood.cpp
@@ -1775,6 +1775,7 @@ int app_main(int argc, char const * const * argv)
     scrSetGameMode(gSetup.fullscreen, gSetup.xdim, gSetup.ydim, gSetup.bpp);
     scrSetGamma(gGamma);
     viewResizeView(gViewSize);
+    vsync = videoSetVsync(vsync);
     initprintf("Initializing sound system\n");
     sndInit();
     sfxInit();
