nukeykt/NBlood

commit 36af1bb2881ac1ec58d6d80142adc12a285fa7d3

Makefile: make using LTO=xx for multithreaded linking work on Windows without the error spew, and default it to 12 threads if unspecified

GNUmakefile
@@ -752,10 +752,12 @@ define BUILDRULE
 
 $$($1_$2)$$(EXESUFFIX): $$(foreach i,$(call getdeps,$1,$2),$$(call expandobjs,$$i)) $$($1_$2_miscdeps) | $$($1_$2_orderonlydeps)
 	$$(LINK_STATUS)
-	$$(RECIPE_IF) $$(LINKER) -o $$@ $$^ $$(GUI_LIBS) $$($1_$2_ldflags) $$(LIBDIRS) $$(LIBS) $$(RECIPE_RESULT_LINK)
+	$$(call MKDIR,"$$(obj)/$$($1_$2)")
+	$$(RECIPE_IF) $$(LINKER) -save-temps=obj -dumpdir $$(obj)/$$($1_$2)/ -o $$@ $$^ $$(GUI_LIBS) $$($1_$2_ldflags) $$(LIBDIRS) $$(LIBS) $$(RECIPE_RESULT_LINK)
 ifeq ($$(PLATFORM),WII)
 ifneq ($$(ELF2DOL),)
 	$$(ELF2DOL) $$@ $$($1_$2)$$(DOLSUFFIX)
+	$$(call RMDIR,"$$(obj)/$$($1_$2)")
 endif
 endif
 ifneq ($$(STRIP),)
@@ -785,7 +787,9 @@ libklzw$(DLLSUFFIX): $(engine_src)/klzw.cpp
 # to debug the tools link phase, make a copy of this rule explicitly replacing % with the name of a tool, such as kextract
 %$(EXESUFFIX): $(tools_obj)/%.$o $(foreach i,tools $(tools_deps),$(call expandobjs,$i))
 	$(LINK_STATUS)
-	$(RECIPE_IF) $(LINKER) -o $@ $^ $(LIBDIRS) $(LIBS) $(RECIPE_RESULT_LINK)
+	$(call MKDIR,"$(tools_obj)/$*")
+	$(RECIPE_IF) $(LINKER) -save-temps=obj -dumpdir $(tools_obj)/$*/ -o $@ $^ $(LIBDIRS) $(LIBS) $(RECIPE_RESULT_LINK)
+	$(call RMDIR,"$(tools_obj)/$*")
 ifneq ($(STRIP),)
 	$(STRIP) $@
 endif
@@ -795,7 +799,7 @@ endif
 
 $(voidwrap_lib): $(foreach i,$(voidwrap),$(call expandobjs,$i))
 	$(LINK_STATUS)
-	$(RECIPE_IF) $(LINKER) -shared -Wl,-soname,$@ -o $@ $^ $(LIBDIRS) $(voidwrap_root)/sdk/redistributable_bin/$(steamworks_lib) $(RECIPE_RESULT_LINK)
+	$(RECIPE_IF) $(LINKER) -save-temps=obj -dumpdir $(voidwrap_obj)/ -shared -Wl,-soname,$@ -o $@ $^ $(LIBDIRS) $(voidwrap_root)/sdk/redistributable_bin/$(steamworks_lib) $(RECIPE_RESULT_LINK)
 ifneq ($(STRIP),)
 	$(STRIP) $@
 endif

Common.mak
@@ -584,7 +584,7 @@ ifneq (0,$(LTO))
     COMPILERFLAGS += -DUSING_LTO
     ifeq (1,$(LTO))
         ifeq (0,$(CLANG))
-            COMMONFLAGS += -flto
+            COMMONFLAGS += -flto=12
         else
             COMMONFLAGS += -flto=thin
         endif

commit 8c30602fe7a13cbd09dbe2bc15d86d5d7898757b

Makefile: Fix FURY=1 build removing the "obj/fury/" folder after linking.
This bug was introduced in 36af1bb2 and caused the subsequent mapster32
linking to fail. The obj dump folder now has "_dump" as suffix.

For regular duke3d builds, this temp folder had the name "obj/eduke32",
and hence did not conflict with the "obj/duke3d" folder.

GNUmakefile
@@ -759,8 +759,8 @@ define BUILDRULE
 
 $$($1_$2)$$(EXESUFFIX): $$(foreach i,$(call getdeps,$1,$2),$$(call expandobjs,$$i)) $$($1_$2_miscdeps) | $$($1_$2_orderonlydeps)
 	$$(LINK_STATUS)
-	$$(call MKDIR,"$$(obj)/$$($1_$2)")
-	$$(RECIPE_IF) $$(LINKER) $$(call LF,$$(obj)/$$($1_$2)) -o $$@ $$^ $$(GUI_LIBS) $$($1_$2_ldflags) $$(LIBDIRS) $$(LIBS) $$(RECIPE_RESULT_LINK)
+	$$(call MKDIR,"$$(obj)/$$($1_$2)_dump")
+	$$(RECIPE_IF) $$(LINKER) $$(call LF,$$(obj)/$$($1_$2)_dump) -o $$@ $$^ $$(GUI_LIBS) $$($1_$2_ldflags) $$(LIBDIRS) $$(LIBS) $$(RECIPE_RESULT_LINK)
 ifeq ($$(PLATFORM),WII)
 ifneq ($$(ELF2DOL),)
 	$$(ELF2DOL) $$@ $$($1_$2)$$(DOLSUFFIX)
@@ -774,7 +774,7 @@ ifeq ($$(PLATFORM),DARWIN)
 	$(call MKDIR,"$$($1_$2_proper).app/Contents/MacOS")
 	cp -f "$$($1_$2)$$(EXESUFFIX)" "$$($1_$2_proper).app/Contents/MacOS/"
 endif
-	$$(call RMDIR,"$$(obj)/$$($1_$2)")
+	$$(call RMDIR,"$$(obj)/$$($1_$2)_dump")
 
 endef
 