nukeykt/NBlood

commit 9070cd2bff6d4547f0765fc483ffd4f923e24383

Fix build failure and warnings if YAX_ENABLE is undefined

source/build/src/engine.cpp
@@ -366,6 +366,7 @@ void yax_updategrays(int32_t posze)
 
     for (i=0; i<numsectors; i++)
     {
+        int32_t keep;
 #ifdef YAX_ENABLE
         int16_t cb, fb;
         yax_getbunches(i, &cb, &fb);
@@ -373,7 +374,7 @@ void yax_updategrays(int32_t posze)
         // Update grayouts due to TROR, has to be --v--       half-open      --v--
         // because only one level should ever be    v                          v
         // active.                                  v                          v
-        int32_t keep = ((cb<0 || sector[i].ceilingz < posze) && (fb<0 || posze <= sector[i].floorz));
+        keep = ((cb<0 || sector[i].ceilingz < posze) && (fb<0 || posze <= sector[i].floorz));
         if (autogray && (cb>=0 || fb>=0) && (sector[i].ceilingz <= posze && posze <= sector[i].floorz))
         {
             mingoodz = min(mingoodz, TrackerCast(sector[i].ceilingz));
@@ -1601,8 +1602,8 @@ char apptitle[256] = "Build Engine";
 //          1=break out of sprite collecting;
 int32_t renderAddTsprite(int16_t z, int16_t sectnum)
 {
-    auto const spr = (uspriteptr_t)&sprite[z];
 #ifdef YAX_ENABLE
+    auto const spr = (uspriteptr_t)&sprite[z];
     if (g_nodraw==0)
     {
         if (numyaxbunches==0)
@@ -1665,6 +1666,8 @@ int32_t renderAddTsprite(int16_t z, int16_t sectnum)
             }
         }
     }
+#else
+    UNREFERENCED_PARAMETER(sectnum);
 #endif
 
     return 0;
@@ -10795,6 +10798,7 @@ void calc_sector_reachability(void)
 #endif
                 }
 
+#ifdef YAX_ENABLE
                 int upperSect = yax_vnextsec(j, YAX_CEILING);
                 if (upperSect >= 0 && !bitmap_test(sectbitmap, upperSect))
                 {
@@ -10812,6 +10816,7 @@ void calc_sector_reachability(void)
                     LOG_F(INFO, "sector %d reaches sector %d through TROR", sectnum, lowerSect);
 #endif
                 }
+#endif
             }
         }
     }
@@ -12728,7 +12733,9 @@ restart_grand:
             if (z <= cfz[0] || z >= cfz[1])
                 return 0;
 
+#ifdef YAX_ENABLE
 add_nextsector:
+#endif
             if (!bitmap_test(sectbitmap, nexts))
             {
                 bitmap_set(sectbitmap, nexts);
@@ -12957,6 +12964,8 @@ void dragpoint(int16_t pointhighlight, int32_t dax, int32_t day, uint8_t flags)
 {
     int16_t cnt, tempshort;
     int32_t thelastwall;
+
+    UNREFERENCED_PARAMETER(flags);
 
     tempshort = pointhighlight;    //search points CCW
     cnt = MAXWALLS;

source/duke3d/src/actors.cpp
@@ -473,14 +473,14 @@ static int32_t Proj_MaybeDoTransport(int32_t spriteNum, uspriteptr_t const pSEff
     return 1;
 }
 
+#ifdef YAX_ENABLE
 // Check whether sprite <s> is on/in a non-SE7 water sector.
 // <othersectptr>: if not NULL, the sector on the other side.
 int A_CheckNoSE7Water(uspriteptr_t const pSprite, int sectNum, int sectLotag, int32_t *pOther)
 {
     if (sectLotag == ST_1_ABOVE_WATER || sectLotag == ST_2_UNDERWATER)
     {
-        int const otherSect =
-        yax_getneighborsect(pSprite->x, pSprite->y, sectNum, sectLotag == ST_1_ABOVE_WATER ? YAX_FLOOR : YAX_CEILING);
+        int const otherSect = yax_getneighborsect(pSprite->x, pSprite->y, sectNum, sectLotag == ST_1_ABOVE_WATER ? YAX_FLOOR : YAX_CEILING);
         int const otherLotag = (sectLotag == ST_1_ABOVE_WATER) ? ST_2_UNDERWATER : ST_1_ABOVE_WATER;
 
         // If submerging, the lower sector MUST have lotag 2.
@@ -497,6 +497,7 @@ int A_CheckNoSE7Water(uspriteptr_t const pSprite, int sectNum, int sectLotag, in
 
     return 0;
 }
+#endif
 
 // Check whether to do a z position update of sprite <spritenum>.
 // Returns:

source/duke3d/src/game.cpp
@@ -701,8 +701,10 @@ void G_HandleMirror(int32_t x, int32_t y, int32_t z, fix16_t a, fix16_t q16horiz
                 yax_preparedrawrooms();
                 didmirror = renderDrawRoomsQ16(tposx,tposy,z,tang,q16horiz,g_mirrorSector[i]+MAXSECTORS);
                 //POGO: if didmirror == 0, we may simply wish to abort instead of rendering with yax_drawrooms (which may require cleaning yax state)
+#ifdef YAX_ENABLE
                 if (videoGetRenderMode() != REND_CLASSIC || didmirror)
                     yax_drawrooms(G_DoSpriteAnimations, g_mirrorSector[i], didmirror, smoothratio);
+#endif
             }
 #ifdef USE_OPENGL
             else

source/build/src/2d.cpp
@@ -1315,7 +1315,7 @@ void editorDraw2dScreen(const vec3_t *pos, int16_t cursectnum, int16_t ange, int
     if (!m32_sideview)
     {
 #ifndef YAX_ENABLE
-        for (i=numwalls-1; i>=0; i--)
+        for (int32_t i = numwalls-1; i >= 0; i--)
             editorDraw2dWall(i, posxe, posye, posze, zoome, 0);
 #else
         if (alwaysshowgray)
@@ -1390,11 +1390,15 @@ void editorDraw2dScreen(const vec3_t *pos, int16_t cursectnum, int16_t ange, int
 
         for (int32_t i = 0; i < m32_swcnt; i++)  // shouldn't it go the other way around?
         {
+            int32_t skipSector;
             int32_t j = m32_wallsprite[i];
             if (j<MAXWALLS)
             {
                 if (skipgraysectors)
-                    YAX_SKIPSECTOR(sectorofwall(j));
+                {
+                    skipSector = sectorofwall(j);
+                    YAX_SKIPSECTOR(skipSector);
+                }
 
                 if (alwaysshowgray || !bitmap_test(graybitmap, j))
                     editorDraw2dWall(j, posxe, posye, posze, zoome, !!bitmap_test(graybitmap, j));
@@ -1402,10 +1406,16 @@ void editorDraw2dScreen(const vec3_t *pos, int16_t cursectnum, int16_t ange, int
             else
             {
                 if (skipgraysectors)
-                    YAX_SKIPSECTOR(sectorofwall(sprite[j-MAXWALLS].sectnum));
+                {
+                    skipSector = sectorofwall(sprite[j-MAXWALLS].sectnum);
+                    YAX_SKIPSECTOR(skipSector);
+                }
 
                 if (!alwaysshowgray && sprite[j-MAXWALLS].sectnum>=0)
-                    YAX_SKIPSECTOR(sprite[j-MAXWALLS].sectnum);
+                {
+                    skipSector = sprite[j-MAXWALLS].sectnum;
+                    YAX_SKIPSECTOR(skipSector);
+                }
 
                 editorDraw2dSprite(j-MAXWALLS, posxe, posye, posze, zoome);
             }

source/build/src/build.cpp
@@ -2898,6 +2898,8 @@ void SetFirstWall(int32_t sectnum, int32_t wallnum, int32_t alsoynw)
 {
 #ifdef YAX_ENABLE
     int32_t i, j, k=0;
+#else
+    UNREFERENCED_PARAMETER(alsoynw);
 #endif
     const sectortype *sec = &sector[sectnum];
 
@@ -3152,6 +3154,8 @@ static int32_t M32_InsertPoint(int32_t thewall, int32_t dax, int32_t day, int16_
             return m|(j<<16);
     }
     else
+#else
+    UNREFERENCED_PARAMETER(onewnumwalls);
 #endif
     {
         insertpoint(thewall, dax,day, mapwallnum);
@@ -6689,8 +6693,8 @@ end_point_dragging:
                         joinsector[1] = i;
 
                         const int s1to0wall = find_nextwall(i, joinsector[0]);
-                        const int s0to1wall = s1to0wall == -1 ? -1 : wall[s1to0wall].nextwall;
 #ifdef YAX_ENABLE
+                        const int s0to1wall = s1to0wall == -1 ? -1 : wall[s1to0wall].nextwall;
                         int16_t jbn[2][2];  // [join index][c/f]
 
                         for (k=0; k<2; k++)
@@ -7755,7 +7759,11 @@ check_next_sector: ;
 
                     if (numwalls==expectedNumwalls)
                     {
+#ifdef YAX_ENABLE
                         if (doSectorSplit && cb<0 && fb<0)
+#else
+                        if (doSectorSplit)
+#endif
                         {
                             if (firstwallflag)
                             {

source/build/src/clip.cpp
@@ -15,10 +15,9 @@
 
 static int16_t clipnum;
 static linetype clipit[MAXCLIPNUM];
-static int32_t clipsectnum, origclipsectnum, layerclipsectnum, clipspritenum;
+static int32_t clipsectnum, origclipsectnum, clipspritenum;
 int16_t clipsectorlist[MAXCLIPSECTORS];
 static int16_t origclipsectorlist[MAXCLIPSECTORS];
-static int16_t layerclipsectorlist[MAXCLIPSECTORS];
 static uint8_t clipsectormap[bitmap_size(MAXSECTORS)];
 static uint8_t origclipsectormap[bitmap_size(MAXSECTORS)];
 #ifdef HAVE_CLIPSHAPE_FEATURE
@@ -26,6 +25,11 @@ static int16_t clipspritelist[MAXCLIPNUM];  // sector-like sprite clipping
 #endif
 static int16_t clipobjectval[MAXCLIPNUM];
 static uint8_t clipignore[bitmap_size(MAXCLIPNUM)];
+
+#ifdef YAX_ENABLE
+static int16_t layerclipsectorlist[MAXCLIPSECTORS];
+static int32_t layerclipsectnum;
+#endif
 
 ////// sector-like clipping for sprites //////
 #ifdef HAVE_CLIPSHAPE_FEATURE

source/build/src/polymer.cpp
@@ -1794,8 +1794,10 @@ static void         polymer_displayrooms(const int16_t dacursectnum)
 {
     usectorptr_t      sec;
     int32_t         i;
+#ifdef YAX_ENABLE
     int16_t         bunchnum;
     int16_t         ns;
+#endif
     GLint           result;
     int16_t         doquery;
     int32_t         front;
@@ -3050,9 +3052,12 @@ static void polymer_drawsector(int16_t sectnum, int32_t domasks)
     // Draw masks regardless; avoid all non-masks TROR links
     if (sec->floorstat & 384) {
         draw = domasks;
-    } else if (yax_getbunch(sectnum, YAX_FLOOR) >= 0) {
+    }
+#ifdef YAX_ENABLE
+    else if (yax_getbunch(sectnum, YAX_FLOOR) >= 0) {
         draw = FALSE;
     }
+#endif
 
     // Parallaxed
     if (sec->floorstat & 1) {
@@ -3096,9 +3101,12 @@ static void polymer_drawsector(int16_t sectnum, int32_t domasks)
     // Draw masks regardless; avoid all non-masks TROR links
     if (sec->ceilingstat & 384) {
         draw = domasks;
-    } else if (yax_getbunch(sectnum, YAX_CEILING) >= 0) {
+    }
+#ifdef YAX_ENABLE
+    else if (yax_getbunch(sectnum, YAX_CEILING) >= 0) {
         draw = FALSE;
     }
+#endif
 
     // Parallaxed
     if (sec->ceilingstat & 1) {
@@ -6017,7 +6025,9 @@ static int polymer_culllight(int16_t lighti)
 
     int32_t front = 0;
     int32_t back  = 1;
+#ifdef YAX_ENABLE
     int16_t bunchnum;
+#endif
 
     if (!sectorsareconnected(globalcursectnum, light.sector)) return 1;
 
@@ -6032,7 +6042,9 @@ static int polymer_culllight(int16_t lighti)
 
         polymer_pokesector(sectorqueue[front]);
 
+#ifdef YAX_ENABLE
         int checkror = FALSE;
+#endif
 
         if (!(sec->floorstat & 1))
         {
@@ -6044,11 +6056,15 @@ static int polymer_culllight(int16_t lighti)
             if (!light.radius) {
                 if (zdiff < light.range) {
                     polymer_addplanelight(&s->floor, lighti);
+#ifdef YAX_ENABLE
                     checkror = TRUE;
+#endif
                 }
             } else if (polymer_planeinlight(s->floor, light)) {
                 polymer_addplanelight(&s->floor, lighti);
+#ifdef YAX_ENABLE
                 checkror = TRUE;
+#endif
             }
         }
 
@@ -6067,8 +6083,8 @@ static int polymer_culllight(int16_t lighti)
                 }
             }
         }
-#endif
         checkror = FALSE;
+#endif
 
         if (!(sec->ceilingstat & 1))
         {
@@ -6080,11 +6096,15 @@ static int polymer_culllight(int16_t lighti)
             if (!light.radius) {
                 if (zdiff < light.range) {
                     polymer_addplanelight(&s->ceil, lighti);
+#ifdef YAX_ENABLE
                     checkror = TRUE;
+#endif
                 }
             } else if (polymer_planeinlight(s->ceil, light)) {
                 polymer_addplanelight(&s->ceil, lighti);
+#ifdef YAX_ENABLE
                 checkror = TRUE;
+#endif
             }
         }
 

source/duke3d/src/actors.h
@@ -409,7 +409,9 @@ extern uint8_t      g_radiusDmgStatnums[bitmap_size(MAXSTATUS)];
 int dukeValidateSectorEffectorPlaysSound(int);
 int dukeValidateSectorPlaysSound(int);
 
+#ifdef YAX_ENABLE
 int  A_CheckNoSE7Water(uspriteptr_t pSprite, int sectNum, int sectLotag, int32_t *pOther);
+#endif
 int  A_CheckSwitchTile(int spriteNum);
 int A_IncurDamage(int spriteNum);
 void A_AddToDeleteQueue(int spriteNum);

commit 25fa3cabd6f3b0cfdbb47d092e7ad6f3e5df3065

Duke3D: Properly split TROR code from commit 19fd0915 into YAX_ENABLE blocks
And fix problems introduced by 9070cd2bff6d4547f0765fc483ffd4f923e24383.

source/build/src/engine.cpp
@@ -366,7 +366,7 @@ void yax_updategrays(int32_t posze)
 
     for (i=0; i<numsectors; i++)
     {
-        int32_t keep;
+        int32_t keep = true;
 #ifdef YAX_ENABLE
         int16_t cb, fb;
         yax_getbunches(i, &cb, &fb);

source/duke3d/src/actors.cpp
@@ -838,7 +838,11 @@ void A_DoGuts(int spriteNum, int tileNum, int spawnCnt)
         repeat.x = repeat.y = 8;
 
     int gutZ   = pSprite->z - ZOFFSET3;
+#ifdef YAX_ENABLE
     int floorz = yax_getflorzofslope(pSprite->sectnum, pSprite->xy);
+#else
+    int floorz = getflorzofslope(pSprite->sectnum, pSprite->x, pSprite->y);
+#endif
 
     if (gutZ > (floorz-ZOFFSET3))
         gutZ = floorz-ZOFFSET3;
@@ -5620,7 +5624,11 @@ ACTOR_STATIC void G_MoveMisc(void)  // STATNUM 5
             switchPic--;
 
         if ((pSprite->picnum == MONEY+1) || (pSprite->picnum == MAIL+1) || (pSprite->picnum == PAPER+1))
-            actor[spriteNum].floorz = pSprite->z = yax_getflorzofslope(pSprite->sectnum,pSprite->xy);
+#ifdef YAX_ENABLE
+            actor[spriteNum].floorz = pSprite->z = yax_getflorzofslope(pSprite->sectnum, pSprite->xy);
+#else
+            actor[spriteNum].floorz = pSprite->z = getflorzofslope(pSprite->sectnum, pSprite->x, pSprite->y);
+#endif
         else
 #endif
         {
@@ -5843,7 +5851,11 @@ ACTOR_STATIC void G_MoveMisc(void)  // STATNUM 5
                 if (pSprite->sectnum == -1)
                     DELETE_SPRITE_AND_CONTINUE(spriteNum);
 
+#ifdef YAX_ENABLE
                 int const floorZ = yax_getflorzofslope(pSprite->sectnum, pSprite->xy);
+#else
+                int const floorZ = getflorzofslope(pSprite->sectnum, pSprite->x, pSprite->y);
+#endif
 
                 if (pSprite->z > floorZ)
                 {

source/duke3d/src/game.cpp
@@ -696,12 +696,13 @@ void G_HandleMirror(int32_t x, int32_t y, int32_t z, fix16_t a, fix16_t q16horiz
 
             if (videoGetRenderMode() != REND_POLYMER)
             {
-                int32_t didmirror;
-
-                yax_preparedrawrooms();
-                didmirror = renderDrawRoomsQ16(tposx,tposy,z,tang,q16horiz,g_mirrorSector[i]+MAXSECTORS);
-                //POGO: if didmirror == 0, we may simply wish to abort instead of rendering with yax_drawrooms (which may require cleaning yax state)
 #ifdef YAX_ENABLE
+                yax_preparedrawrooms();
+                auto const didmirror =
+#endif
+                    renderDrawRoomsQ16(tposx,tposy,z,tang,q16horiz,g_mirrorSector[i]+MAXSECTORS);
+#ifdef YAX_ENABLE
+                //POGO: if didmirror == 0, we may simply wish to abort instead of rendering with yax_drawrooms (which may require cleaning yax state)
                 if (videoGetRenderMode() != REND_CLASSIC || didmirror)
                     yax_drawrooms(G_DoSpriteAnimations, g_mirrorSector[i], didmirror, smoothratio);
 #endif
@@ -2754,7 +2755,11 @@ int A_Spawn(int spriteNum, int tileNum)
 
             if (spriteNum >= 0)
             {
+#ifdef YAX_ENABLE
                 int const floorZ = yax_getflorzofslope(pSprite->sectnum, pSprite->xy);
+#else
+                int const floorZ = getflorzofslope(pSprite->sectnum, pSprite->x, pSprite->y);
+#endif
 
                 if (pSprite->z > floorZ-ZOFFSET4)
                     pSprite->z = floorZ-ZOFFSET4;