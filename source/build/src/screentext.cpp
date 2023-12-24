nukeykt/NBlood

commit 5997fee3c6b9c2bda5aa31e8a80782f3901c4be1

engine: respect xbetween value for screentext between chars in constant width mode

source/build/src/screentext.cpp
@@ -141,7 +141,7 @@ vec2_t screentextGetSize(ScreenTextSize_t const & data)
             vec2_16_t const siz = tilesiz[tile];
 
             // width
-            extent.x = constwidthactive ? constwidth : siz.x * data.zoom;
+            extent.x = constwidthactive ? constwidth + xbetween : siz.x * data.zoom;
 
             // height
             SetIfGreater(&extent.y, (siz.y * data.zoom));
@@ -389,7 +389,7 @@ vec2_t screentextRender(ScreenText_t const & data)
             rotatesprite_(location.x, location.y, data.zoom, angle, tile, data.shade, pal, o, alpha, blendidx, data.b1.x, data.b1.y, data.b2.x, data.b2.y);
 
             // width
-            extent.x = constwidthactive ? constwidth : siz.x * data.zoom;
+            extent.x = constwidthactive ? constwidth + xbetween : siz.x * data.zoom;
 
             // height
             SetIfGreater(&extent.y, (siz.y * data.zoom));

commit 84204758904b33787220400501f71b5b82fb9708

engine: fix screentext problem introduced in 5997fee3c6b9c2bda5aa31e8a80782f3901c4be1 by folding the relevant change into the new screentext flag introduced in 03a39f4d64a9131138eeecab08003d5c6b67da1c

source/build/src/screentext.cpp
@@ -141,7 +141,7 @@ vec2_t screentextGetSize(ScreenTextSize_t const & data)
             vec2_16_t const siz = tilesiz[tile];
 
             // width
-            extent.x = constwidthactive ? constwidth + xbetween : siz.x * data.zoom;
+            extent.x = constwidthactive ? constwidth + (data.f & TEXT_CENTERCONSTWIDTH ? xbetween : 0) : siz.x * data.zoom;
 
             // height
             SetIfGreater(&extent.y, (siz.y * data.zoom));
@@ -392,7 +392,7 @@ vec2_t screentextRender(ScreenText_t const & data)
             rotatesprite_(location.x, location.y, data.zoom, angle, tile, data.shade, pal, o, alpha, blendidx, data.b1.x, data.b1.y, data.b2.x, data.b2.y);
 
             // width
-            extent.x = constwidthactive ? constwidth + xbetween : siz.x * data.zoom;
+            extent.x = constwidthactive ? constwidth + (data.f & TEXT_CENTERCONSTWIDTH ? xbetween : 0) : siz.x * data.zoom;
 
             // height
             SetIfGreater(&extent.y, (siz.y * data.zoom));