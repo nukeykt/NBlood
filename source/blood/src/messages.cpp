nukeykt/NBlood

commit 961a063957ca2b9b73db88adf6847f7ffc805253

Always use modern method to clear game messages
This fixes a message draw glitch when finishing an episode, returning to menu with a demo playing in the background, and starting a new episode

source/blood/src/messages.cpp
@@ -447,16 +447,19 @@ void CGameMessageMgr::Display(void)
 
 void CGameMessageMgr::Clear(void)
 {
+#if 0 // we have the CPU cycles with current-day hardware to delete every message now, don't use this old method
     if (VanillaMode())
     {
         messagesIndex = nextMessagesIndex = numberOfDisplayedMessages = 0;
     }
     else
+#endif
     {
         for (int i = 0; i < kMessageLogSize; i++)
         {
             messageStruct* pMessage = &messages[i];
             pMessage->deleted = true;
+            pMessage->lastTickWhenVisible = 0;
         }
     }
 }

commit 60daf224c4ed842f05f76aec35ca05dcb1f60580

Fix issue introduced in 961a063957ca2b9b73db88adf6847f7ffc805253
This improve the robustness for clearing out messages when switching from non-vanilla mode to vanilla mode

source/blood/src/messages.cpp
@@ -450,20 +450,16 @@ void CGameMessageMgr::Display(void)
 
 void CGameMessageMgr::Clear(void)
 {
+    messagesIndex = nextMessagesIndex = numberOfDisplayedMessages = 0;
 #if 0 // we have the CPU cycles with current-day hardware to delete every message now, don't use this old method
     if (VanillaMode())
-    {
-        messagesIndex = nextMessagesIndex = numberOfDisplayedMessages = 0;
-    }
-    else
+        return;
 #endif
+    for (int i = 0; i < kMessageLogSize; i++)
     {
-        for (int i = 0; i < kMessageLogSize; i++)
-        {
-            messageStruct* pMessage = &messages[i];
-            pMessage->deleted = true;
-            pMessage->lastTickWhenVisible = 0;
-        }
+        messageStruct* pMessage = &messages[i];
+        pMessage->deleted = true;
+        pMessage->lastTickWhenVisible = 0;
     }
 }
 