b6bf756102047c548208a1e3f089582215504211
sirlemonhead <sirlemonhead@outlook.com>
2024-05-12 12:24:47 +0100

    PCExhumed: Add missing cmd arg for loading additional defs

--- source/exhumed/src/exhumed.cpp
+++ source/exhumed/src/exhumed.cpp
@@ -2035,6 +2035,14 @@ int app_main(int argc, char const* const* argv)
                     i++;
                 }
             }
+            else if (Bstrcasecmp(pChar, "mh") == 0)
+            {
+                if (argc > i + 1)
+                {
+                    G_AddDefModule(argv[i + 1]);
+                    i++;
+                }
+            }
             else
             {
                 char c = tolower(*pChar);
